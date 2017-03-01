28da49ce5884ba183a8e6fd67fb92d491a1df437
Andy Meerwaldt <andy@yoast.com>
2017-03-01 11:46:10 +0100

    Added the default button

---
+++ src/components/Button.js
@@ -0,0 +1,15 @@
+
+import styled from "styled-components";
+
+import colors from "yoast-components/style-guide/colors.json";
+
+export const Button = styled.button`
+	background: ${colors.$palette_blue};
+	color: ${colors.$palette_white};
+	box-shadow: 0px 2px 2px 2px rgba(0,0,0,0.1);
+	border-radius: 5px;
+	font-size: 14px;
+	text-transform: uppercase;
+	padding: 15px;
+	border: 0;
+`;
