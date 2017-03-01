31d4b5c24d7a7e5d909daaa0e99becefdabcb5b3
Thijs Hulshof <thijsh@yoast.com>
2017-03-01 16:12:15 +0100

    Cleaned up css and resized logo

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -3,11 +3,10 @@ import styled from "styled-components";
 import logo from "../img/My_Yoast_logo.svg";
 
 export const Logo = styled.div`
-    padding-top:'10px';
+    padding-top: 10px;
     background-image: url( ${logo});
     background-position: center;
-    background-size: 70%;
-    left:0px; top: 0xp;
+    background-size: 55%;
     width: 100%;
     height: 20%;
     background-repeat: no-repeat;
