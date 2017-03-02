0b12e30807e51d9f6b6cc8a862808230a904e620
Andy Meerwaldt <andy@yoast.com>
2017-03-02 09:12:49 +0100

    Change the button size

--- src/components/RoundButton.js
+++ src/components/RoundButton.js
@@ -12,24 +12,21 @@ export const RoundButton = styled.button`
 	background-repeat: no-repeat;
 	box-shadow: 0px 2px 2px 2px rgba(0, 0, 0, 0.1);
 	border: 0;
+	border-radius: 100%;
+	background-size: 30px;
+	width:  40px;
+	height: 40px;
 `;
 
 export const RoundBackButton = styled(RoundButton)`
 	background-image: url( ${angleLeft} );
 	background-position: 4px center;
-	background-size: 30px;
-	border-radius: 40px;
-	width:  40px;
-	height: 40px;
 `;
 
 export const RoundAddButton = styled(RoundButton)`
 	background-image: url( ${plus} );
 	background-position: center;
-	background-size: 25px;
-	border-radius: 60px;
-	width:  60px;
-	height: 60px;
+	background-size: 20px;
 `;
 
 RoundButton.propTypes = {
