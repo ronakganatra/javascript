b091577abc87dddde2b90b1ac097d23e6e8a6f47
Thijs Hulshof <thijsh@yoast.com>
2017-03-02 13:32:58 +0100

    Added props for Logo component

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -3,13 +3,14 @@ import styled from "styled-components";
 import logo from "../img/My_Yoast_logo.svg";
 
 export const Logo = styled.div`
-    padding-top: 10px;
+    padding: 10px;
+    height: ${props=>props.height};
+    width: ${props=>props.width};
     background-image: url( ${logo});
+    background-color: black;
     background-position: center;
-    background-size: 55%;
-    width: 100%;
-    height: 20%;
     background-repeat: no-repeat;
+    background-size: auto;
 `;
 
 export default Logo;
