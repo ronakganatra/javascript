55463ad403cde1ed20ece08f6dedd51769110abd
boblinthorst <bob.linthorst@gmail.com>
2017-03-03 12:23:52 +0100

    Changed names, made parenthesis usage consistent and removed black background

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,13 +1,11 @@
-import React from "react";
 import styled from "styled-components";
-import logo from "../img/My_Yoast_logo.svg";
+import logo from "../images/logo.svg";
 
 export const Logo = styled.div`
     padding: 10px;
     height: ${props=>props.height};
     width: ${props=>props.width};
-    background-image: url( ${logo});
-    background-color: black;
+    background-image: url( ${logo} );
     background-position: center;
     background-repeat: no-repeat;
     background-size: auto;
