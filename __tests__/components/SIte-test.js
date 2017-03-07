da03da13f9d5b0e244bbd04111beb3df9297e0a8
Andy Meerwaldt <andy@yoast.com>
2017-03-07 11:17:22 +0100

    Add test for Site component.

---
+++ __tests__/components/SIte-test.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import Site from '../../src/components/Site';
+
+test('the site component matches the snapshot', () => {
+	const component = renderer.create(
+		<Site activeSubscriptions={ [ "woo", "video" ] } siteName="yoast.com" siteIcon="" />
+	);
+
+	let tree = component.toJSON();
+	expect(tree).toMatchSnapshot();
+});
+
+test('the manage button handling an onclick event', () => {
+	const component = renderer.create(
+		<Site
+			activeSubscriptions={ [ "woo", "video" ] }
+			siteName="yoast.com"
+			siteIcon=""
+			onClickManage={ () => {
+				return 'clicked';
+		} } />
+	);
+
+	let tree = component.toJSON();
+	expect(tree).toMatchSnapshot();
+
+	// manually trigger the callback
+	tree.children[4].props.onClick();
+
+	// re-rendering
+	tree = component.toJSON();
+	expect(tree).toMatchSnapshot();
+});
