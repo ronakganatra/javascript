e71118142c139c5dc7de47f59eaef7260faa0e3b
Thijs Hulshof <thijsh@yoast.com>
2017-03-09 21:08:13 +0100

    Improved styling

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,40 +1,39 @@
 import React from "react";
 import styled from "styled-components";
 import colors from "yoast-components/style-guide/colors.json";
-import { Link, NavLink } from 'react-router-dom';
+import { NavLink } from "react-router-dom";
 
 const activeStyle = btoa( Math.random() );
 
 const items = [
-	{item:'/subscriptions', title:'Subscriptions'},
-	{item:'/sites', title:'Sites'},
-	{item:'/courses', title:'Courses'},
-	{item:'/account', title:'Account'}
+	{ item: "/subscriptions", title: "Subscriptions" },
+	{ item: "/sites", title: "Sites" },
+	{ item: "/courses", title: "Courses" },
+	{ item: "/account", title: "Account" },
 ];
 
 const Menu = styled.div`
-	width: 300px;
 `;
 
-const MenuItem = styled(NavLink)`
+const MenuItem = styled( NavLink )`
 	display: block;
 	height: 100px;
 	line-height: 100px;
 	font: "Open Sans";
 	font-size: 22px;
 	font-weight: 300;
-	width: 250px;
 	padding-left: 25px;
 	margin-left: 25px;
-	color: ${colors.$color_pink_dark};
+	color: ${colors.$color_background_light};
 	text-decoration: none;
 	
-	&.${activeStyle} {
+	&.${ activeStyle } {
 		color: #ccc;
 		background-color: ${colors.$background};
 		box-shadow: inset 0px 2px 8px 0px rgba(0, 0, 0, 0.3);
 		font: "Open Sans";
 		font-weight: 400;
+		color: ${colors.$color_pink_dark};
 	}
 `;
 
@@ -42,39 +41,40 @@ export const MainMenu = () => {
 	return (
 		<Menu>
 			<nav>
-				{items.map((page) => {
-					return <MenuItem activeClassName={activeStyle} to={page.item} key={page.title}>{page.title}</MenuItem>}
-				)}
+				{ items.map( ( page ) => {
+					return <MenuItem activeClassName={ activeStyle } to={ page.item } key={ page.title }>{ page.title }</MenuItem>;
+				}
+				) }
 			</nav>
 		</Menu>
-	)
+	);
 };
 
-export const Home = React.createClass({
+export const Home = React.createClass( {
 	render: function() {
-		return (<h1>Welcome to the Home Page</h1>);
-	}
-});
+		return ( <h1>Welcome to the Home Page</h1> );
+	},
+} );
 
-export const Subscriptions = React.createClass({
+export const Subscriptions = React.createClass( {
 	render: function() {
-		return (<h1>Your subscriptions</h1>);
-	}
-});
+		return ( <h1>Your subscriptions</h1> );
+	},
+} );
 
-export const Sites = React.createClass({
+export const Sites = React.createClass( {
 	render: function() {
-		return (<h1>Your sites</h1>);
-	}
-});
+		return ( <h1>Your sites</h1> );
+	},
+} );
 
-export const Courses = React.createClass({
+export const Courses = React.createClass( {
 	render: function() {
-		return (<h1>Your courses</h1>);
-	}
-});
-export const Account = React.createClass({
+		return ( <h1>Your courses</h1> );
+	},
+} );
+export const Account = React.createClass( {
 	render: function() {
-		return (<h1>Your account details</h1>);
-	}
-});
+		return ( <h1>Your account details</h1> );
+	},
+} );
