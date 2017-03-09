6d1ba0de61c9a35bac224bd27a31e64fc474fb71
Thijs Hulshof <thijsh@yoast.com>
2017-03-09 08:38:14 +0100

    Added styling for active button

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,71 +1,44 @@
 import React from "react";
 import styled from "styled-components";
 import colors from "yoast-components/style-guide/colors.json";
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
+import './Menu.css';
 
 const items = [
-    {item:'Subscriptions'},
-    {item:'Sites'},
-    {item:'Courses'},
-    {item:'Account'}
+    {item:'/subscriptions', title:'Subscriptions'},
+    {item:'/sites', title:'Sites'},
+    {item:'/courses', title:'Courses'},
+    {item:'/account', title:'Account'}
 ];
-
-const rando = btoa(Math.random());
-
 
 const Menu = styled.div`
     width: 300px;
-    height: 40px;
-    lineHeight: 40px;
-    paddingLeft: 10px;  
-    textDecoration:none;
-    color: white;
-
 `;
 
-const MenuItem = styled.div`
-    padding-left: 10px;
-    margin-top: 5px;
-    margin-bottom: 5px;
-    width: 100px;
-    height: 40px;
-    line-height: 40px;
-    color: grey; 
-`;
+const MenuItem = styled.a`
+    display: block;
+    width: 270px;
+    height: 100px;
+    padding-left: 30px;
+    font-family: 'open sans 300';
+    font-size: 22px;
+    color: #ccc;
+    text-decoration: none;
+    a, a:visited {color:red; text-decoration: none;};
 
-const StyledLink = styled(Link)`
-    text-decoration: none; 
-    color: white;
-    &:visited: red;
-    &.active {
-    text-decoration: underline red;
-    }
-    ;
 `;
 
 export const MainMenu = () => {
     return (
         <Menu>
-            <nav><MenuItem>{items.map((page) => {
-                return <div><StyledLink to={page.item} activeClassName="active">{page.item}</StyledLink></div>}
+            <nav>
+                {items.map((page) => {
+                return <NavLink activeClassName="menuItemActive" to={page.item}><MenuItem>{page.title}</MenuItem></NavLink>}
             )}
-            </MenuItem>
             </nav>
         </Menu>
     )
 };
-
-
-//
-// export const MainMenu = () => {
-//     return (
-//         <div id="menu">
-//             {items.map((page) => {
-//                 return <div className="menuItem"><Link to={page.item}>{page.item}</Link></div>}
-//             )}
-//         </div>
-//     )
-// };
 
 export const Home = React.createClass({
     render: function() {
@@ -95,49 +68,3 @@ export const Account = React.createClass({
         return (<h1>Your account details</h1>);
     }
 });
-//
-// export const items = [
-//     {item:'Subscriptions'},
-//     {item:'Sites'}
-// ];
-//
-// export const Menu = ({items}) => (
-//     <div id="menu">
-//         {items.map(page => (
-//             <div className="item" key={page.item}><Link to="{page.item}">Link {page.item}</Link></div>
-//         ))}
-//     </div>
-// );
-
-
-
-
-export const MenuNav = styled.nav`
-    display: block;
-`;
-
-// export const MenuItem = styled.div`
-//     background-color: ${colors.$palette_pink_dark};
-//     box-sizing: border-box;
-//     padding-left: 10px;
-//     margin-top: 5px;
-//     margin-bottom: 5px;
-//     width: ${props=>props.width};
-//     height: 40px;
-//     line-height: 40px;
-//     color: ${colors.$color_grey};
-// `;
-
-export const MenuItemSelected = styled.div`
-    background-color: ${colors.$color_background_light};
-    height: 20px;
-    padding-left: 10px;
-    margin-top: 5px;
-    margin-bottom: 5px;
-    width: ${props=>props.width};
-    height: 40px;
-    line-height: 40px;
-    font-weight: bold;
-    box-shadow: inset 1px 1px 1px 1px darkgrey;
-    color: ${colors.$palette_pink_dark};
-`;
