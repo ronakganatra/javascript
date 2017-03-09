b73dcfe95c3bbd83220c73dcfa2e37fe64d5c972
Andrea Fercia <a.fercia@gmail.com>
2017-03-09 12:35:20 +0100

    Use injectGlobal for body styling.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
+/* eslint-disable no-unused-expressions */
 import React, { Component } from "react";
 import "./App.css";
 import "normalize.css/normalize.css";
 import UserStatus from "./containers/UserStatus";
 import { Layout, Sidebar, Content } from "./components/Layout";
 import { Provider } from "react-redux";
+import { injectGlobal } from "styled-components";
+import colors from "yoast-components/style-guide/colors.json";
+
+/*
+ * Helper method to write global CSS.
+ * Only use it for the rare @font-face definition or body styling.
+ */
+injectGlobal`
+	body {
+		margin: 0;
+		padding: 0;
+		font-family: "Open Sans", sans-serif;
+		min-height: 100%;
+		background: ${colors.$background};
+	}
+`;
 
 class App extends Component {
 	render() {
