c8447518275dfd0d183be74dfea74aa6365d66a9
Thijs Hulshof <thijsh@yoast.com>
2017-03-09 14:01:21 +0100

    Fixed indentation

--- src/App.js
+++ src/App.js
@@ -9,43 +9,27 @@ import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 class App extends Component {
 	render() {
-        const routes = [
-            { path: '/',
-                exact: true,
-                sidebar: () => <div>home!</div>,
-                main: () => <h2>Home</h2>
-            },
-            { path: '/bubblegum',
-                sidebar: () => <div>bubblegum!</div>,
-                main: () => <h2>Bubblegum</h2>
-            },
-            { path: '/shoelaces',
-                sidebar: () => <div>shoelaces!</div>,
-                main: () => <h2>Shoelaces</h2>
-            }
-        ]
-
 		return (
 
-        <Provider store={this.props.store}>
-            <Router>
-                <Layout>
-                    <Sidebar>
-                        <UserStatus/>
+		<Provider store={this.props.store}>
+			<Router>
+				<Layout>
+					<Sidebar>
+						<UserStatus/>
 
-                        <MainMenu/>
-                    </Sidebar>
+						<MainMenu/>
+					</Sidebar>
 
-                    <Content>
-                        <Route path="/subscriptions" component={Subscriptions} />
-                        <Route path="/courses" component={Courses} />
-                        <Route path="/sites" component={Sites} />
-                        <Route path="/account" component={Account} />
-                    </Content>
-                </Layout>
-            </Router>
+					<Content>
+						<Route path="/subscriptions" component={Subscriptions} />
+						<Route path="/courses" component={Courses} />
+						<Route path="/sites" component={Sites} />
+						<Route path="/account" component={Account} />
+					</Content>
+				</Layout>
+			</Router>
 			{/*<Router>*/}
-                {/*<Layout>*/}
+				{/*<Layout>*/}
 					{/*<Route component={Sidebar}>*/}
 						{/*<Route component={UserStatus} />*/}
 
@@ -57,7 +41,7 @@ class App extends Component {
 						{/*<Route path="sites" component={Sites} />*/}
 						{/*<Route path="account" component={Account} />*/}
 					{/*</Route>*/}
-                {/*</Layout>*/}
+				{/*</Layout>*/}
 			{/*</Router>*/}
 		</Provider>
 		);
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -6,74 +6,75 @@ import { Link, NavLink } from 'react-router-dom';
 const activeStyle = btoa( Math.random() );
 
 const items = [
-    {item:'/subscriptions', title:'Subscriptions'},
-    {item:'/sites', title:'Sites'},
-    {item:'/courses', title:'Courses'},
-    {item:'/account', title:'Account'}
+	{item:'/subscriptions', title:'Subscriptions'},
+	{item:'/sites', title:'Sites'},
+	{item:'/courses', title:'Courses'},
+	{item:'/account', title:'Account'}
 ];
 
 const Menu = styled.div`
-    width: 300px;
+	width: 300px;
 `;
 
 const MenuItem = styled(NavLink)`
-    display: block;
-    height: 100px;
-    line-height: 100px;
-    font: "Open Sans";
-    font-size: 22px;
-    font-weight: 300;
-    width: 250px;
-    padding-left: 25px;
-    margin-left: 25px;
-    color: ${colors.$color_pink_dark};
-    text-decoration: none;
-    
-    &.${activeStyle} {
-        color: #ccc;
-        box-shadow: inset 0px 2px 8px 0px rgba(0, 0, 0, 0.3);
-        font: "Open Sans";
-        font-weight: 400;
-    }
+	display: block;
+	height: 100px;
+	line-height: 100px;
+	font: "Open Sans";
+	font-size: 22px;
+	font-weight: 300;
+	width: 250px;
+	padding-left: 25px;
+	margin-left: 25px;
+	color: ${colors.$color_pink_dark};
+	text-decoration: none;
+	
+	&.${activeStyle} {
+		color: #ccc;
+		background-color: ${colors.$background};
+		box-shadow: inset 0px 2px 8px 0px rgba(0, 0, 0, 0.3);
+		font: "Open Sans";
+		font-weight: 400;
+	}
 `;
 
 export const MainMenu = () => {
-    return (
-        <Menu>
-            <nav>
-                {items.map((page) => {
-                    return <MenuItem activeClassName={activeStyle} to={page.item} key={page.title}>{page.title}</MenuItem>}
-                )}
-            </nav>
-        </Menu>
-    )
+	return (
+		<Menu>
+			<nav>
+				{items.map((page) => {
+					return <MenuItem activeClassName={activeStyle} to={page.item} key={page.title}>{page.title}</MenuItem>}
+				)}
+			</nav>
+		</Menu>
+	)
 };
 
 export const Home = React.createClass({
-    render: function() {
-        return (<h1>Welcome to the Home Page</h1>);
-    }
+	render: function() {
+		return (<h1>Welcome to the Home Page</h1>);
+	}
 });
 
 export const Subscriptions = React.createClass({
-    render: function() {
-        return (<h1>Your subscriptions</h1>);
-    }
+	render: function() {
+		return (<h1>Your subscriptions</h1>);
+	}
 });
 
 export const Sites = React.createClass({
-    render: function() {
-        return (<h1>Your sites</h1>);
-    }
+	render: function() {
+		return (<h1>Your sites</h1>);
+	}
 });
 
 export const Courses = React.createClass({
-    render: function() {
-        return (<h1>Your courses</h1>);
-    }
+	render: function() {
+		return (<h1>Your courses</h1>);
+	}
 });
 export const Account = React.createClass({
-    render: function() {
-        return (<h1>Your account details</h1>);
-    }
+	render: function() {
+		return (<h1>Your account details</h1>);
+	}
 });
