e8105abd72dd6ed2bc0adc63db973d5f25e01155
Andrea Fercia <a.fercia@gmail.com>
2017-03-10 14:09:50 +0100

    Fix LargeButton size and add WhiteButton.

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,20 +1,18 @@
 import React from "react";
-
 import styled from "styled-components";
-
 import colors from "yoast-components/style-guide/colors.json";
 
-import angleRight from "../icons/angle-right.svg";
-
 export const Button = styled.button`
+	height: 48px;
+	padding: 0 15px;
+	border: 0;
 	background-color: ${colors.$color_blue};
 	color: ${colors.$color_white};
 	box-shadow: 0px 2px 2px 2px rgba(0, 0, 0, 0.1);
-	border-radius: 5px;
+	border-radius: 3px;
 	font-size: 14px;
 	text-transform: uppercase;
-	padding: 15px;
-	border: 0;
+	cursor: pointer;
 `;
 
 Button.propTypes = {
@@ -27,14 +25,14 @@ Button.defaultProps = {
 };
 
 export const LargeButton = styled( Button )`
-	font-size: 16px;
-	padding: 20px 40px 20px 20px;
-	background-image: url( ${angleRight} );
-	background-size: 40px;
-	background-position: center right;
-	background-repeat: no-repeat;
+	min-width: 150px;
 `;
 
 export const GreenButton = styled( Button )`
 	background-color: ${colors.$color_green};
+`;
+
+export const WhiteButton = styled( LargeButton )`
+	color: ${colors.$color_blue};
+	background-color: ${colors.$color_white};
 `;
