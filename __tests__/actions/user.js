507dbb1ae8c9d3d91fa3135fd55a1506c85956e9
Anton Timmermans <email@atimmer.com>
2017-03-13 16:11:49 +0100

    Add tests for reducers & actions

---
+++ __tests__/actions/user.js
@@ -0,0 +1,74 @@
+import * as actions from "../../src/actions/user";
+
+jest.mock( "whatwg-fetch" );
+
+test( 'login action creator', () => {
+	const expected = {
+		type: actions.LOGIN,
+		data: {
+			accessToken: "Access Token",
+			userId: 5
+		},
+	};
+
+	const actual = actions.login( "Access Token", 5 );
+
+	expect( actual ).toEqual( expected );
+} );
+
+test( 'logout action creator', () => {
+	const expected = {
+		type: actions.LOGOUT,
+	};
+
+	const actual = actions.logout();
+
+	expect( actual ).toEqual( expected );
+} );
+
+test( 'request user action creator', () => {
+	const expected = {
+		type: actions.FETCH_USER_REQUEST,
+	};
+
+	const actual = actions.requestUser();
+
+	expect( actual ).toEqual( expected );
+} );
+
+
+test( 'request user action creator', () => {
+	const expected = {
+		type: actions.FETCH_USER_SUCCESS,
+		user: {
+			userData: "data",
+		},
+	};
+	const input = {
+		userData: "data",
+	};
+
+	const actual = actions.receiveUser( input );
+
+	expect( actual ).toEqual( expected );
+} );
+
+test( 'fetch user action creator', () => {
+	global.fetch = jest.fn( () => {
+		return Promise.resolve( {
+			json: () => { return "User data" },
+		} );
+	});
+
+	const dispatch = jest.fn();
+
+	const fetchUserFunc = actions.fetchUser( "AccessToken", 5 );
+
+	expect( fetchUserFunc ).toBeInstanceOf( Function );
+
+	return fetchUserFunc( dispatch ).then( () => {
+		expect( dispatch ).toHaveBeenCalledWith( actions.requestUser() );
+		expect( global.fetch ).toHaveBeenCalledWith( "http://localhost:3000/api/MyYoastUsers/5?access_token=AccessToken" );
+		expect( dispatch ).toHaveBeenCalledWith( actions.receiveUser( "User data" ) );
+	} );
+} );
