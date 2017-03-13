19203e42d70d8cd8c957b2ed631179eece3c68c2
Andrea Fercia <a.fercia@gmail.com>
2017-03-13 16:58:44 +0100

    Implement SkipLink component.

---
+++ src/components/SkipLink.js
@@ -0,0 +1,33 @@
+import React from "react";
+import styled from "styled-components";
+import colors from "yoast-components/style-guide/colors.json";
+
+export const SkipLink = styled.a`
+	position: absolute;
+	top: -1000em;
+	left: 10px;
+	z-index: 100000;
+	display: block;
+	height: auto;
+	width: auto;
+	padding: 1em;
+	font-size: 1em;
+	background: ${colors.$color_white};
+	color: ${colors.$color_blue};
+	border-radius: 3px;
+	box-shadow: 0 0 2px 2px rgba(0,0,0,.6);
+	text-decoration: underline;
+	outline: none;
+
+	&:focus {
+		top: 10px;
+	}
+`;
+
+SkipLink.propTypes = {
+	href: React.PropTypes.string.isRequired,
+};
+
+SkipLink.defaultProps = {
+	href: "#content",
+};
