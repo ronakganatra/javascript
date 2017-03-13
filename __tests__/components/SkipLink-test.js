199692bd69b1cd3f299d72e59f290bf793521c3c
Andrea Fercia <a.fercia@gmail.com>
2017-03-13 17:05:15 +0100

    Added test.

---
+++ __tests__/components/SkipLink-test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import { SkipLink } from '../../src/components/SkipLink';
+
+test('the skip link matches the snapshot', () => {
+	const component = renderer.create(
+		<SkipLink>LinkText</SkipLink>
+	);
+
+	let tree = component.toJSON();
+	expect(tree).toMatchSnapshot();
+});
