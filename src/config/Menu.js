e5a99d2bf3b5c83004af32b24fadad4a79cc3b3a
Andrea Fercia <a.fercia@gmail.com>
2017-03-14 16:35:00 +0100

    Add a11y messages.

--- src/config/Menu.js
+++ src/config/Menu.js
@@ -1,9 +1,13 @@
 import React from "react";
 import NoSites from "../components/NoSites";
+import a11ySpeak from "a11y-speak";
 
 const Subscriptions = React.createClass( {
 	render: function() {
 		return ( <h1>Your subscriptions</h1> );
+	},
+	componentDidMount: function() {
+		a11ySpeak( "Subscriptions page loaded" );
 	},
 } );
 
@@ -11,17 +15,26 @@ const Sites = React.createClass( {
 	render: function() {
 		return ( <NoSites onClick={() => {}} /> );
 	},
+	componentDidMount: function() {
+		a11ySpeak( "Sites page loaded" );
+	},
 } );
 
 const Courses = React.createClass( {
 	render: function() {
 		return ( <h1>Your courses</h1> );
 	},
+	componentDidMount: function() {
+		a11ySpeak( "Courses page loaded" );
+	},
 } );
 
 const Account = React.createClass( {
 	render: function() {
 		return ( <h1>Your account details</h1> );
+	},
+	componentDidMount: function() {
+		a11ySpeak( "Account details page loaded" );
 	},
 } );
 
