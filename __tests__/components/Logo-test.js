bff75915b3335acaad046d45a14ab68fc67949fb
boblinthorst <bob.linthorst@gmail.com>
2017-03-14 10:00:48 +0100

    Processed code review

--- __tests__/components/Logo-test.js
+++ __tests__/components/Logo-test.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import Logo from '../../src/components/Logo';
+import { Logo } from '../../src/components/Logo';
+import { HeaderLogo } from '../../src/components/Logo';
 
 test('the logo matches the snapshot', () => {
     const component = renderer.create(
-        <Logo />
+        <Logo size="300px" alt="My logo alt" />
     );
 
     let tree = component.toJSON();
     expect(tree).toMatchSnapshot();
 });
+
+test('the header logo matches the snapshot', () => {
+    const component = renderer.create(
+        <HeaderLogo size="300px" alt="My header logo alt" />
+    );
+
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+});
+
+
--- src/components/Logo.js
+++ src/components/Logo.js
@@ -5,23 +5,49 @@ import logo from "../images/logo.svg";
 /**
  * @param {Object} props Component props.
  * @param {String} props.size The width of the prop, and therefore the size.
+ * @param {String} props.alt The alternative text to show for the logo.
+ * @returns {ReactElement} A react component describing the logo component for the header
+ * @constructor
+ */
+export function HeaderLogo( props ) {
+	return (
+		<header role="banner">
+			<Logo size={ props.size } alt={ props.alt }/>
+		</header>
+	);
+}
+
+HeaderLogo.propTypes = {
+	size: React.PropTypes.string,
+	alt: React.PropTypes.string,
+};
+
+HeaderLogo.defaultProps = {
+	size: "200px",
+	alt: "My Yoast home",
+};
+
+/**
+ * @param {Object} props Component props.
+ * @param {String} props.size The width of the prop.
+ * @param {String} props.alt The alternative text to show for the logo.
  * @returns {ReactElement} A react component describing the logo component.
  * @constructor
  */
-export default function Logo( props ) {
+export function Logo( props ) {
 	return (
-		<header role="banner">
-			<LogoImage src={ logo } size={ props.size } alt="My Yoast"/>
-		</header>
+		<LogoImage src={ logo } size={ props.size } alt={ props.alt }/>
 	);
 }
 
 Logo.propTypes = {
 	size: React.PropTypes.string,
+	alt: React.PropTypes.string,
 };
 
 Logo.defaultProps = {
 	size: "200px",
+	alt: "My Yoast home",
 };
 
 const LogoImage = styled.img`
