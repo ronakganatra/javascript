a0cb0cde02d2968e66e9301f53d907f122b24980
Andrea Fercia <a.fercia@gmail.com>
2017-03-15 10:36:56 +0100

    Messages i18n.

--- src/config/Menu.js
+++ src/config/Menu.js
@@ -1,42 +1,83 @@
 import React from "react";
 import NoSites from "../components/NoSites";
 import a11ySpeak from "a11y-speak";
+import { defineMessages, injectIntl, intlShape } from "react-intl";
 
-const Subscriptions = React.createClass( {
+const messages = defineMessages( {
+	subscriptionsPageLoaded: {
+		id: "menu.subscriptions.loaded",
+		defaultMessage: "Subscriptions page loaded",
+	},
+	sitesPageLoaded: {
+		id: "menu.sites.loaded",
+		defaultMessage: "Sites page loaded",
+	},
+	coursesPageLoaded: {
+		id: "menu.courses.loaded",
+		defaultMessage: "Courses page loaded",
+	},
+	accountPageLoaded: {
+		id: "menu.account.loaded",
+		defaultMessage: "Account page loaded",
+	},
+} );
+
+let Subscriptions = React.createClass( {
 	render: function() {
 		return ( <h1>Your subscriptions</h1> );
 	},
 	componentDidMount: function() {
-		a11ySpeak( "Subscriptions page loaded" );
+		let message = this.props.intl.formatMessage( messages.subscriptionsPageLoaded );
+		a11ySpeak( message );
+	},
+	propTypes: {
+		intl: intlShape.isRequired,
 	},
 } );
 
-const Sites = React.createClass( {
+let Sites = React.createClass( {
 	render: function() {
 		return ( <NoSites onClick={() => {}} /> );
 	},
 	componentDidMount: function() {
-		a11ySpeak( "Sites page loaded" );
+		let message = this.props.intl.formatMessage( messages.sitesPageLoaded );
+		a11ySpeak( message );
+	},
+	propTypes: {
+		intl: intlShape.isRequired,
 	},
 } );
 
-const Courses = React.createClass( {
+let Courses = React.createClass( {
 	render: function() {
 		return ( <h1>Your courses</h1> );
 	},
 	componentDidMount: function() {
-		a11ySpeak( "Courses page loaded" );
+		let message = this.props.intl.formatMessage( messages.coursesPageLoaded );
+		a11ySpeak( message );
+	},
+	propTypes: {
+		intl: intlShape.isRequired,
 	},
 } );
 
-const Account = React.createClass( {
+let Account = React.createClass( {
 	render: function() {
 		return ( <h1>Your account details</h1> );
 	},
 	componentDidMount: function() {
-		a11ySpeak( "Account details page loaded" );
+		let message = this.props.intl.formatMessage( messages.accountPageLoaded );
+		a11ySpeak( message );
+	},
+	propTypes: {
+		intl: intlShape.isRequired,
 	},
 } );
+
+Subscriptions = injectIntl( Subscriptions );
+Sites = injectIntl( Sites );
+Courses = injectIntl( Courses );
+Account = injectIntl( Account );
 
 let menuItems = [
 	{ path: "/subscriptions", title: "Subscriptions", component: Subscriptions },
