133e36f1a993a9863969bb0e90d98244dc59fa36
Andrea Fercia <a.fercia@gmail.com>
2017-03-15 16:45:30 +0100

    Use the White Button style.

--- src/components/SkipLink.js
+++ src/components/SkipLink.js
@@ -8,15 +8,17 @@ const SkipLink = styled.a`
 	left: 10px;
 	z-index: 100000;
 	display: block;
-	height: auto;
-	width: auto;
-	padding: 1em;
-	font-size: 1em;
+	height: 48px;
+	min-width: 150px;
+	padding: 0 15px;
+	font-size: 14px;
+	line-height: 48px;
 	background: ${colors.$color_white};
 	color: ${colors.$color_blue};
 	border-radius: 3px;
-	box-shadow: 0 0 2px 2px rgba(0,0,0,.6);
+	box-shadow: 0px 2px 2px 2px rgba(0, 0, 0, 0.1);
 	text-decoration: underline;
+	text-align: center;
 	outline: none;
 
 	&:focus {
