a10511dee5bf90cc806a3b7c02c9fd30207d836d
boblinthorst <bob.linthorst@gmail.com>
2017-03-21 10:19:24 +0100

    removed additional rounding on buttons

--- src/components/Button.js
+++ src/components/Button.js
@@ -36,7 +36,6 @@ export const TextButton = styled( Button )`
 	width: ${ props => props.buttonWidth };
 	height: 40px;
 	margin: 5px 0px 5px 10px;
-	border-radius: 8px;
 `;
 
 TextButton.PropTypes = {
