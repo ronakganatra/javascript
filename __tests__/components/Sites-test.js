bd34e07992ce09f078eb624d03df7be10c1e25f8
IreneStr <irene@yoast.com>
2017-03-27 14:28:03 +0200

    Add tests for Sites.js and update other tests

---
+++ __tests__/components/Sites-test.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { shallow } from 'enzyme';
+
+import Sites from '../../src/components/Sites';
+
+test('the sites component matches the snapshot', () => {
+	const component = renderer.create(
+		<Sites onClick={ ( sitesId ) => {
+			return sitesId;
+		} } sites={[
+			{ id: "7e54b616-59a7-4389-af3e-c2e0c093b955",
+				siteName: "www.yoast.com",
+				activeSubscriptions: [ "woo", "video" ],
+				siteIcon: "https://yoast-mercury.s3.amazonaws.com/uploads/2013/02/Yoast_Icon_Large_RGB.png",
+			},
+			{ id: "7e54b616-59a7-4389-af3e-c2e0c093b954",
+				siteName: "www.google.com",
+				activeSubscriptions: [ "woo", "video", "local" ],
+				siteIcon: "https://yoast-mercury.s3.amazonaws.com/uploads/2013/02/Yoast_Icon_Large_RGB.png",
+			},
+			{ id: "7e54b616-59a7-4389-af3e-c2e0c093b956",
+				siteName: "www.facebook.com",
+				activeSubscriptions: [ "woo", "video", "news" ],
+				siteIcon: "https://yoast-mercury.s3.amazonaws.com/uploads/2013/02/Yoast_Icon_Large_RGB.png",
+			},
+		] } />
+	);
+
+	let tree = component.toJSON();
+	expect(tree).toMatchSnapshot();
+});
+
+test('the manage button handling an onclick event', () => {
+	const component = shallow(
+		<Sites onClick={ ( sitesId ) => {
+			return sitesId;
+		} } sites={[
+			{ id: "7e54b616-59a7-4389-af3e-c2e0c093b955",
+				siteName: "www.yoast.com",
+				activeSubscriptions: [ "woo", "video" ],
+				siteIcon: "https://yoast-mercury.s3.amazonaws.com/uploads/2013/02/Yoast_Icon_Large_RGB.png",
+			},
+		] } />
+	);
+
+	expect( component.props().children[0].props.onClickManage() ).toEqual( "7e54b616-59a7-4389-af3e-c2e0c093b955" );
+});
