96c1f6d37ce46890c8d9e3064782ed99c1461bfa
IreneStr <irene@yoast.com>
2017-03-31 11:11:52 +0200

    Add SitesPage tests

---
+++ __tests__/components/SitesPage-test.js
@@ -0,0 +1,87 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import SitesPage from '../../src/components/SitesPage';
+import { createComponentWithIntl } from "../../utils";
+import { shallow } from 'enzyme';
+
+test('the sites page component with no sites matches the snapshot', () => {
+	const component = createComponentWithIntl(
+		<SitesPage sites={ [] } addSite={ () => {} } changeSearchQuery={ () => {} } />
+	);
+
+	let tree = component.toJSON();
+	expect( tree ).toMatchSnapshot();
+} );
+
+test('the sites page component with a site matches the snapshot', () => {
+	const site = {
+		id: "7e54b616-59a7-4389-af3e-c2e0c093b955",
+		siteName: "www.yoast.com",
+		activeSubscriptions: [ "woo", "video" ],
+		siteIcon: "https://yoast-mercury.s3.amazonaws.com/uploads/2013/02/Yoast_Icon_Large_RGB.png",
+	};
+	const component = createComponentWithIntl(
+		<SitesPage sites={ [ site ] } addSite={ () => {} } changeSearchQuery={ () => {} } />
+	);
+
+	let tree = component.toJSON();
+	expect( tree ).toMatchSnapshot();
+} );
+
+test('the sites page component without sites handling an onclick event on the add site button', () => {
+	const component = createComponentWithIntl(
+		<SitesPage sites={ [] } addSite={ () => { return 'Add site'; } } changeSearchQuery={ () => {} }  />
+	);
+
+	let tree = component.toJSON();
+	expect( tree ).toMatchSnapshot();
+
+	// manually trigger the callback.
+	tree.children[0].children[4].props.onClick();
+
+	// re-rendering
+	tree = component.toJSON();
+	expect( tree ).toMatchSnapshot();
+} );
+
+test('the sites page component with sites handling an onclick event on the add site button', () => {
+	const component = createComponentWithIntl(
+		<SitesPage sites={ [
+			{ id: "7e54b616-59a7-4389-af3e-c2e0c093b955",
+				siteName: "www.yoast.com",
+				activeSubscriptions: [ "woo", "video" ],
+				siteIcon: "https://yoast-mercury.s3.amazonaws.com/uploads/2013/02/Yoast_Icon_Large_RGB.png",
+			},] } addSite={ () => { return 'Add site'; } } changeSearchQuery={ () => {} } />
+	);
+
+	let tree = component.toJSON();
+	expect( tree ).toMatchSnapshot();
+
+	// manually trigger the callback.
+	tree.children[0].children[1].props.onClick();
+
+	// re-rendering
+	tree = component.toJSON();
+	expect( tree ).toMatchSnapshot();
+} );
+
+test('the sites page component with sites handling a changed search query', () => {
+	const component = createComponentWithIntl(
+		<SitesPage sites={ [
+			{ id: "7e54b616-59a7-4389-af3e-c2e0c093b955",
+				siteName: "www.yoast.com",
+				activeSubscriptions: [ "woo", "video" ],
+				siteIcon: "https://yoast-mercury.s3.amazonaws.com/uploads/2013/02/Yoast_Icon_Large_RGB.png",
+			},] } addSite={ () => {} } changeSearchQuery={ () => { return 'Query changed'; } } />
+	);
+
+	let tree = component.toJSON();
+	expect( tree ).toMatchSnapshot();
+
+	// manually trigger the callback.
+	tree.children[0].children[0].children[1].props.onChange();
+
+	// re-rendering
+	tree = component.toJSON();
+	expect( tree ).toMatchSnapshot();
+} );
