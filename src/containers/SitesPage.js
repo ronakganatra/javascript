a60ea0335924895e22a5c802ffd368175629bdf5
Maarten Leenders <maarten@yoast.com>
2017-04-04 09:21:23 +0200

    Remove comments and console.logs.

--- src/containers/SitesPage.js
+++ src/containers/SitesPage.js
@@ -3,28 +3,6 @@ import { linkSitePopupClose, linkSitePopupOpen, linkSite, linkSiteRequest } from
 import SitesPage from "../components/SitesPage";
 
 const mapStateToProps = ( state ) => {
-	console.log( state );
-
-	console.log( state.ui.sites );
-
-//	let sites = [
-//		{ id: "7e54b616-59a7-4389-af3e-c2e0c093b955",
-//			siteName: "www.yoast.com",
-//			activeSubscriptions: [ "woo", "video" ],
-//			siteIcon: "https://yoast-mercury.s3.amazonaws.com/uploads/2013/02/Yoast_Icon_Large_RGB.png",
-//		},
-//		{ id: "7e54b616-59a7-4389-af3e-c2e0c093b954",
-//			siteName: "www.google.com",
-//			activeSubscriptions: [ "woo", "video", "local" ],
-//			siteIcon: "https://yoast-mercury.s3.amazonaws.com/uploads/2013/02/Yoast_Icon_Large_RGB.png",
-//		},
-//		{ id: "7e54b616-59a7-4389-af3e-c2e0c093b956",
-//			siteName: "www.facebook.com",
-//			activeSubscriptions: [ "woo", "video", "news" ],
-//			siteIcon: "https://yoast-mercury.s3.amazonaws.com/uploads/2013/02/Yoast_Icon_Large_RGB.png",
-//		},
-//	];
-
 	let allIds = state.entities.sites.allIds;
 
 	let sites = allIds.map( ( siteId ) => {
