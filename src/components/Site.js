f019e38fc6128f1f1aa38b511868da5bc514c70b
boblinthorst <bob.linthorst@gmail.com>
2017-04-05 16:47:55 +0200

    Added active subscriptions

--- src/components/Site.js
+++ src/components/Site.js
@@ -8,31 +8,7 @@ import SiteIcon from "./SiteIcon";
 import { ColumnIcon } from "./ColumnIcon";
 import { Row, ColumnText, Column } from "./Tables";
 import { injectIntl, intlShape, defineMessages } from "react-intl";
-
-// const SiteContainer = styled.li`
-// 	background-color: ${colors.$color_white};
-// 	height: 100px;
-// 	display: flex;
-// 	padding-right: 40px;
-// 	align-items: center;
-//
-// 	@media screen and ( max-width: 1355px ) {
-// 		justify-content: space-between;
-// 	}
-// `;
-// const SiteIcon = styled.img`
-// 	height: 48px;
-// 	padding-right: 40px;
-// 	padding-left: 40px;
-// 	border-right: 2px solid ${colors.$color_grey};
-// 	flex: 0 0 128px;
-//
-// 	@media screen and ( max-width: 1355px ) {
-// 		padding-right: 20px;
-// 		padding-left: 20px;
-// 		flex: 0 0 68px;
-// 	}
-// `;
+import SiteSubscriptions from "./SiteSubscriptions";
 
 const messages = defineMessages( {
 	siteName: {
@@ -53,39 +29,6 @@ SiteIcon.propTypes = {
 	src: React.PropTypes.string.isRequired,
 };
 
-// const SiteName = styled.span`
-// 	height: 60px;
-// 	font-size: 14px;
-// 	line-height: 60px;
-// 	overflow: hidden;
-// 	text-overflow: ellipsis;
-// 	flex: 0 0 380px;
-// 	padding-left: 40px;
-// 	white-space: nowrap
-//
-// 	@media screen and ( max-width: 1355px ) {
-// 		padding-left: 20px;
-// 		flex: 1 0 380px;
-// 	}
-//
-// 	@media screen and ( max-width: 660px ) {
-// 		flex: 1 1 380px;
-// 	}
-// `;
-
-// const SiteSubscriptionsContainer = styled.span`
-// 	height: 60px;
-// 	line-height: 60px;
-// 	display: inline-flex;
-// 	align-items: center;
-// 	flex: 1 0 300px;
-// 	font-size: 14px;
-//
-// 	@media screen and ( max-width: 1355px ) {
-// 		display: none;
-// 	}
-// `;
-
 /**
  * Returns the rendered Site component.
  *
@@ -103,24 +46,11 @@ function Site( props ) {
 	delete rowProps.activeSubscriptions;
 	delete rowProps.onClickManage;
 	return (
-		// <SiteContainer>
-		// 	<SiteIcon src={ props.siteIcon } alt="" />
-		// 	<SiteName className="site-name">{ props.siteName }</SiteName>
-		// 	<SiteSubscriptionsContainer className="active-subscriptions">
-		// 		<SiteSubscriptions activeSubscriptions={ props.activeSubscriptions } />
-		// 	</SiteSubscriptionsContainer>
-		// 	<MediaQuery query="(min-width: 1356px)">
-		// 		<LargeButton onClick={ props.onClickManage }>Manage</LargeButton>
-		// 	</MediaQuery>
-		// 	<MediaQuery query="(max-width: 1355px)">
-		// 		<ChevronButton aria-label="Manage" onClick={ props.onClickManage } />
-		// 	</MediaQuery>
-		// </SiteContainer>
 		<Row key={ props.id } { ...rowProps }>
 			<ColumnIcon separator={ true }><SiteIcon src={ props.siteIcon } alt=""/></ColumnIcon>
 			<ColumnText label={ props.intl.formatMessage( messages.siteName ) }>{ props.siteName }</ColumnText>
 			<ColumnText hideOnMobile={ true } hideOnTablet={ true } label={ props.intl.formatMessage( messages.activeSubscriptions ) }
-						ColumnWidth="100px">{ props.intl.formatMessage( messages.activeSubscriptions ) }</ColumnText>
+						ColumnWidth="500px"><SiteSubscriptions activeSubscriptions={ props.activeSubscriptions } /></ColumnText>
 			<Column textAlign="right">
 				<MediaQuery query="(min-width: 1356px)">
 					<LargeButton aria-label={ props.intl.formatMessage( messages.manage ) }
