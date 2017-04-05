133687319936698cdadc0d3f5ae39279bbd1358b
IreneStr <irene@yoast.com>
2017-04-05 16:01:04 +0200

    Add documentation

--- src/components/ListToggle.js
+++ src/components/ListToggle.js
@@ -3,11 +3,11 @@ import styled from "styled-components";
 import colors from "yoast-components/style-guide/colors.json";
 import { SVGIcon } from "./SVGIcon";
 
-const ListToggleContainer = styled.div`
+const CollapsibleHeaderContainer = styled.div`
 	margin-top: 20px;
 `;
 
-const ListToggleHeader = styled.button`
+const CollapsibleHeader = styled.button`
 	display: flex;
 	justify-content: space-between;
 	align-items: center;
@@ -15,7 +15,7 @@ const ListToggleHeader = styled.button`
 	padding: 10px 40px;
 	border: none;
 	background-color: white;
-	cursor: pointer;
+	cursor: pointer; ////////////////////////////////////////////// not working
 
 	h2 {
 		font-weight: 500;
@@ -33,6 +33,13 @@ const ListToggleBody = styled.ul`
 `;
 
 export default class ListToggle extends React.Component {
+	/**
+	 * The constructor.
+	 *
+	 * @constructor
+	 *
+	 * @param {Object} props The props to use.
+	 */
 	constructor( props ) {
 		super( props );
 
@@ -43,16 +50,31 @@ export default class ListToggle extends React.Component {
 		this.toggleOpen = this.toggleOpen.bind( this );
 	}
 
+	/**
+	 * Returns whether the list is collapsed.
+	 *
+	 * @returns {Boolean} True when the list is collapsed.
+	 */
 	isOpen() {
 		return this.state.isOpen;
 	}
 
+	/**
+	 * Toggles whether the list is collapsed.
+	 *
+	 * @returns {void}
+	 */
 	toggleOpen() {
 		this.setState( {
 			isOpen: ! this.state.isOpen,
 		} );
 	}
 
+	/**
+	 * Gets the correct arrow based on whether the list is collapsed or not.
+	 *
+	 * @returns {ReactElement} The upArrow when the header is collapsed, otherwise the downArrow.
+	 */
 	getArrow() {
 		let upArrow = <SVGIcon><path d="M7.41 15.41L12 10.83l4.59 4.58L18 14l-6-6-6 6z"/></SVGIcon>;
 		let downArrow = <SVGIcon><path d="M7.41 7.84L12 12.42l4.59-4.58L18 9.25l-6 6-6-6z"/></SVGIcon>;
@@ -60,6 +82,11 @@ export default class ListToggle extends React.Component {
 		return this.isOpen() ? upArrow : downArrow;
 	}
 
+	/**
+	 * Gets the list when it is collapsed.
+	 *
+	 * @returns {ReactElement} The list.
+	 */
 	getList() {
 		let body;
 
@@ -70,22 +97,27 @@ export default class ListToggle extends React.Component {
 		return body;
 	}
 
+	/**
+	 * Returns the rendered ListToggle element.
+	 *
+	 * @returns {ReactElement} The rendered ListToggle element.
+	 */
 	render() {
 		return (
-			<ListToggleContainer>
-				<ListToggleHeader onClick={ this.toggleOpen }>
+			<CollapsibleHeaderContainer>
+				<CollapsibleHeader onClick={ this.toggleOpen }>
 					<h2>{ this.props.title }</h2>
 					{ this.getArrow() }
-				</ListToggleHeader>
+				</CollapsibleHeader>
 				{ this.getList() }
-			</ListToggleContainer>
+			</CollapsibleHeaderContainer>
 		);
 	}
 }
 
 ListToggle.propTypes = {
 	title: React.PropTypes.string.isRequired,
-	items: React.PropTypes.any,
+	items: React.PropTypes.any.isRequired
 	isOpen: React.PropTypes.bool,
 };
 
