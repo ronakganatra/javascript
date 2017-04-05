b26ffc1d78fa39e7e45e702cf05525a0c5724012
IreneStr <irene@yoast.com>
2017-04-05 14:26:56 +0200

    Add subscription props to test

--- __tests__/components/Subscriptions-test.js
+++ __tests__/components/Subscriptions-test.js
@@ -6,7 +6,28 @@ import { MemoryRouter } from "react-router-dom";
 test('The Subscriptions component matches the snapshot', () => {
 	const component = createComponentWithIntl(
 		<MemoryRouter>
-			<Subscriptions subscriptions={ subscriptions } />
+			<Subscriptions subscriptions= {[
+				{
+					id: "3",
+					icon: "https://yoast-mercury.s3.amazonaws.com/uploads/2013/02/Yoast_Icon_Large_RGB.png",
+					name: "SEO Premium for WordPress",
+					used: 4,
+					max: 20,
+					nextBilling: new Date("October 13, 2014"),
+					billingAmount: 125.12,
+					billingCurrency: "$",
+				},
+				{
+					id: "4",
+					icon: "https://yoast-mercury.s3.amazonaws.com/uploads/2013/02/Yoast_Icon_Large_RGB.png",
+					name: "SEO Premium for WordPress",
+					used: 3,
+					max: 20,
+					nextBilling: new Date("October 13, 2014"),
+					billingAmount: 125.12,
+					billingCurrency: "$",
+				},
+			] } />
 		</MemoryRouter>
 			);
 
