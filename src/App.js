c337fbbd7e35d95133bbcd76462a1dcc762ca8af
boblinthorst <bob.linthorst@gmail.com>
2017-04-07 09:27:34 +0200

    Updated tests

--- src/App.js
+++ src/App.js
@@ -13,10 +13,6 @@ import colors from "yoast-components/style-guide/colors.json";
 import { IntlProvider } from "react-intl";
 import DebugInfo from "./components/DebugInfo";
 import { Logo } from "./components/Logo";
-import SitesPage from "./components/SitesPage";
-import { Table, Zebra } from "./components/Tables";
-import Paper from "./components/Paper";
-import Subscription from "./components/Subscription";
 
 /*
  * Helper method to write global CSS.
@@ -52,45 +48,6 @@ class App extends Component {
 								<Content>
 									<Route exact path="/" component={ Sites } />
 									<MainMenuRoutes menuRoutes={ menuItems }  />
-									<SitesPage sites={ [
-										{ id: "7e54b616-59a7-4389-af3e-c2e0c093b955",
-											siteName: "www.yoast.com",
-											activeSubscriptions: [ "woo", "video" ],
-											siteIcon: "https://yoast-mercury.s3.amazonaws.com/uploads/2013/02/Yoast_Icon_Large_RGB.png",
-										},
-										{ id: "7e54b616-59a7-4389-af3e-c2e0c093b954",
-											siteName: "www.google.com",
-											activeSubscriptions: [ "woo", "video", "local" ],
-											siteIcon: "https://yoast-mercury.s3.amazonaws.com/uploads/2013/02/Yoast_Icon_Large_RGB.png",
-										},
-										{ id: "7e54b616-59a7-4389-af3e-c2e0c093b956",
-											siteName: "www.facebook.com",
-											activeSubscriptions: [ "woo", "video", "news" ],
-											siteIcon: "https://yoast-mercury.s3.amazonaws.com/uploads/2013/02/Yoast_Icon_Large_RGB.png",
-										},
-									] } addSite={ () => {
-										console.log( "add" );
-									} } changeSearchQuery={ () => {
-										console.log( "changed" );
-									} } />
-
-									<Table>
-										<Paper>
-											<Zebra>
-												<Subscription id="a" icon="https://yoast-mercury.s3.amazonaws.com/uploads/2013/02/Yoast_Icon_Large_RGB.png" name="SEO Premium for WordPress"
-															  used={ 14 } max={ 20 }
-															  nextBilling={ new Date() }
-															  billingAmount={ 125.12 }
-															  billingCurrency="$" />
-												<Subscription id="b" icon="https://yoast-mercury.s3.amazonaws.com/uploads/2013/02/Yoast_Icon_Large_RGB.png" name="SEO Premium for WordPress"
-															  used={ 4 } max={ 10 }
-															  nextBilling={ new Date() }
-															  billingAmount={ 125.12 }
-															  billingCurrency="$" />
-											</Zebra>
-										</Paper>
-									</Table>
-
 								</Content>
 							</Main>
 						</Layout>
