9f2ec6534d23f1b89e5da902abd0843c48740fb6
IreneStr <irene@yoast.com>
2017-04-11 16:09:12 +0200

    Add siteSubscriptions

--- src/components/SitePage.js
+++ src/components/SitePage.js
@@ -2,10 +2,11 @@ import React from "react";
 import styled from "styled-components";
 import a11ySpeak from "a11y-speak";
 import { defineMessages, injectIntl, intlShape } from "react-intl";
+import { Link } from "react-router-dom";
 
 import SiteHeader from "./SiteHeader";
 import { RoundBackButton } from "./RoundButton";
-// import SiteSubscriptionDetailList from "./SiteSubscriptionDetailList";
+import SiteSubscriptionDetailList from "./SiteSubscriptionDetailList";
 
 const messages = defineMessages( {
 	sitePageLoaded: {
@@ -14,9 +15,10 @@ const messages = defineMessages( {
 	},
 } );
 
-const BackToSitesButton = styled( RoundBackButton )`
-	margin: 0 0 36px -80px;
+const SitePageContainer = styled.div`
+	display: inline;
 `;
+
 
 /**
  * Returns the rendered Site Page component.
@@ -35,13 +37,23 @@ class SitePage extends React.Component {
 		let props = this.props;
 
 		return (
-			<div>
-					<BackToSitesButton onClick={ () => {
-						console.log( "clicked" );
-					} }
-					/>
-					<SiteHeader name={ props.siteName } url={ props.siteUrl } imageUrl={ props.siteImage }/>
-			</div>
+			<SitePageContainer>
+				<Link to={ "/sites" } >
+					<RoundBackButton />
+				</Link>
+				<SiteHeader name={ props.site.url } url={ props.site.url } imageUrl={ props.siteImage }/>
+				<SiteSubscriptionDetailList siteSubscriptions={ props.subscriptions }
+											onToggleSubscription={ props.onToggleSubscription }
+											onMoreInfoClick={ props.onMoreInfoClick }
+											onSettingsClick={ props.onSettingsClick }
+											slots={ {
+												amountAvailable: props.slots.amountAvailable,
+												amountUsed: props.slots.amountUsed,
+												onAddMoreSlotsClick: props.slots.onAddMoreSlotsClick,
+												addMoreSlots: props.slots.addMoreSlots,
+											} }
+				/>
+			</SitePageContainer>
 		);
 	}
 }
@@ -52,9 +64,15 @@ SitePage.propTypes = {
 	onToggleSubscription: React.PropTypes.func.isRequired,
 	site: React.PropTypes.object.isRequired,
 	subscriptions: React.PropTypes.arrayOf( React.PropTypes.object ),
-	siteName: React.PropTypes.string.isRequired,
-	siteUrl:React.PropTypes.string.isRequired,
 	siteImage: React.PropTypes.string.isRequired,
+	onMoreInfoClick: React.PropTypes.func.isRequired,
+	onSettingsClick: React.PropTypes.func.isRequired,
+	slots: React.PropTypes.shape( {
+		amountAvailable: React.PropTypes.number.isRequired,
+		amountUsed: React.PropTypes.number,
+		onAddMoreSlotsClick: React.PropTypes.func,
+		addMoreSlots: React.PropTypes.string,
+	} ).isRequired,
 	intl: intlShape.isRequired,
 };
 
--- src/components/SiteSubscriptionDetailList.js
+++ src/components/SiteSubscriptionDetailList.js
@@ -1,5 +1,6 @@
 import React from "react";
 import CollapsibleHeader from "./CollapsibleHeader";
+import SiteSubscriptionDetail from "./SiteSubscriptionDetail";
 import { injectIntl, intlShape, defineMessages } from "react-intl";
 
 const messages = defineMessages( {
@@ -18,12 +19,40 @@ const messages = defineMessages( {
  */
 function SiteSubscriptionDetailList( props ) {
 	return (
-		<CollapsibleHeader title={ props.intl.formatMessage( messages.manageTitle ) } items={ props.siteSubscriptions } isOpen={ true } />
+		<CollapsibleHeader title={ props.intl.formatMessage( messages.manageTitle ) } items={ props.siteSubscriptions } isOpen={ true }>
+			{ props.siteSubscriptions.map( ( subscription ) => {
+				return <SiteSubscriptionDetail { ...subscription }
+											   key={ subscription.productId }
+											   onMoreInfoClick={ props.onMoreInfoClick }
+											   onSettingsClick={ props.onSettingsClick }
+											   onToggleSubscription={ () => {
+												   console.log( "toggled" );
+											   } }
+											   slots={ {
+												   amountAvailable: subscription.slots.amountAvailable,
+												   amountUsed: subscription.slots.amountUsed,
+												   onAddMoreSlotsClick: subscription.slots.onAddMoreSlotsClick,
+												   addMoreSlots: subscription.slots.addMoreSlots,
+											   } }
+											   productLogo={ subscription.productLogo }
+				/>;
+			} ) }
+		</CollapsibleHeader>
 	);
 }
 
 SiteSubscriptionDetailList.propTypes = {
 	siteSubscriptions: React.PropTypes.array,
+	onToggleSubscription: React.PropTypes.func.isRequired,
+	onMoreInfoClick: React.PropTypes.func.isRequired,
+	onSettingsClick: React.PropTypes.func.isRequired,
+	slots: React.PropTypes.shape( {
+		amountAvailable: React.PropTypes.number.isRequired,
+		amountUsed: React.PropTypes.number,
+		onAddMoreSlotsClick: React.PropTypes.func,
+		addMoreSlots: React.PropTypes.string,
+	} ).isRequired,
+	productLogo: React.PropTypes.string.isRequired,
 	intl: intlShape.isRequired,
 };
 
