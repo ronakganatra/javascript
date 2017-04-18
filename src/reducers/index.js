072cf1a67ca1d055e8e9d8baeb4e66e85d42b0d8
Thijs Hulshof <thijsh@yoast.com>
2017-04-18 13:28:51 +0200

    Created entitiesOrdersReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,11 +3,12 @@ import { combineReducers } from "redux";
 import { uiSitesReducer, byIdReducer, allIdsReducer } from "./sites.js";
 import { routerReducer } from "react-router-redux";
 import { allIdsSitesSubscriptionsReducer, byIdSitesSubscriptionsReducer, uiSiteSubscriptionsReducer } from "./subscriptions.js";
-
+import { uiOrdersReducer, byIdOrdersReducer, allIdsOrdersReducer } from "./orders";
 
 export const uiReducer = combineReducers( {
 	sites: uiSitesReducer,
 	site: uiSiteSubscriptionsReducer,
+	orders: uiOrdersReducer,
 } );
 
 export const entitiesSitesReducer = combineReducers( {
@@ -20,9 +21,15 @@ export const entitiesSubscriptionsReducer = combineReducers( {
 	allIds: allIdsSitesSubscriptionsReducer,
 } );
 
+export const entitiesOrdersReducer = combineReducers( {
+	byId: byIdOrdersReducer,
+	allIds: allIdsOrdersReducer,
+} );
+
 export const entitiesReducer = combineReducers( {
 	sites: entitiesSitesReducer,
 	subscriptions: entitiesSubscriptionsReducer,
+	orders: entitiesOrdersReducer,
 } );
 
 export const rootReducer = combineReducers( {
