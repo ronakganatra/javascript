d5062e67275877e3fa321793e61f11448e17c2aa
IreneStr <irene@yoast.com>
2017-04-20 09:26:41 +0200

    Add tables

--- src/components/SubscriptionDetails.js
+++ src/components/SubscriptionDetails.js
@@ -1,44 +1,22 @@
-/**
- *
- * @param {Object} props The props to use
- *
- * @returns {ReactElement} The rendered component.
- */
 import React from "react";
 import styled from "styled-components";
 import colors from "yoast-components/style-guide/colors.json";
 import { ColumnText, Row, Table, Zebra } from "./Tables";
-import Paper from "./Paper";
 
 const SubscriptionDetailsContainer = styled.div`
-	height: 300px;
+	div:nth-child(even) {
+		padding-left: 10px;
+	}
+	div:nth-child(odd) {
+		padding-right: 10px;
+	}
 	background-color: ${ colors.$color_white };
 `;
 
-/**
- *
- * @param {Object} props The props to use
- *
- * @returns {ReactElement} The rendered paymentDetailsTable component.
- */
-export function paymentDetailsTable( props ) {
-	return (
-		<Table  headings={ false } role="list" >
-			<Paper>
-				<Zebra>
-					<Row key="1" >
-						<ColumnText>{ "Start date" }</ColumnText>
-						<ColumnText>{ "7 October 2017" }</ColumnText>
-					</Row>
-					<Row key="2" >
-						<ColumnText>{ "Start date" }</ColumnText>
-						<ColumnText>{ "8 October 2017" }</ColumnText>
-					</Row>
-				</Zebra>
-			</Paper>
-		</Table>
-	);
-}
+const ColumnContainer = styled.div`
+	width: 50%
+	float: left;
+`;
 
 /**
  *
@@ -51,27 +29,55 @@ export function paymentDetailsTable( props ) {
 export default function SubscriptionDetails( props ) {
 	return (
 		<SubscriptionDetailsContainer>
-			<paymentDetailsTable />
+			<ColumnContainer>
+				<Table  headings={ false } role="list" >
+					<Zebra>
+						<Row key="1" >
+							<ColumnText>{props.startDate}</ColumnText>
+							<ColumnText>{ "7 October 2017" }</ColumnText>
+						</Row>
+						<Row key="2" >
+							<ColumnText>{ "Start date" }</ColumnText>
+							<ColumnText>{ "8 October 2017" }</ColumnText>
+						</Row>
+					</Zebra>
+				</Table>
+			</ColumnContainer>
+			<ColumnContainer>
+				<Table  headings={ false } role="list" >
+					<Zebra>
+						<Row key="1" >
+							<ColumnText>{props.startDate}</ColumnText>
+							<ColumnText>{ "7 October 2017" }</ColumnText>
+						</Row>
+						<Row key="2" >
+							<ColumnText>{ "Start date" }</ColumnText>
+							<ColumnText>{ "8 October 2017" }</ColumnText>
+						</Row>
+					</Zebra>
+				</Table>
+			</ColumnContainer>
+			<ColumnContainer>
+				<Table  headings={ false } role="list" >
+					<Zebra>
+						<Row key="1" >
+							<ColumnText>{props.startDate}</ColumnText>
+							<ColumnText>{ "7 October 2017" }</ColumnText>
+						</Row>
+						<Row key="2" >
+							<ColumnText>{ "Start date" }</ColumnText>
+							<ColumnText>{ "8 October 2017" }</ColumnText>
+						</Row>
+					</Zebra>
+				</Table>
+			</ColumnContainer>
 		</SubscriptionDetailsContainer>
 	);
 }
 
-paymentDetailsTable.propTypes = {
-	subscriptions: React.PropTypes.arrayOf(
-		React.PropTypes.shape(
-			{
-				id: React.PropTypes.string.isRequired,
-				icon: React.PropTypes.string.isRequired,
-				name: React.PropTypes.string.isRequired,
-				used: React.PropTypes.number.isRequired,
-				max: React.PropTypes.number.isRequired,
-				nextBilling: React.PropTypes.instanceOf( Date ).isRequired,
-				billingAmount: React.PropTypes.number.isRequired,
-				billingCurrency: React.PropTypes.string.isRequired,
-			}
-		)
-	),
+SubscriptionDetails.propTypes = {
+	startDate: React.PropTypes.string,
 };
 
-paymentDetailsTable.defaultProps = {
+SubscriptionDetails.defaultProps = {
 };
