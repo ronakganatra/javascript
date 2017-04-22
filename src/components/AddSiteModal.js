fbf60c1add787a1457105deae541eb9bba63779a
Jip Moors <jhp.moors@gmail.com>
2017-04-22 11:43:25 +0200

    Impove modal behaviour
    Use the maximum amount of space available with max-width and max-height.
    Compensate for submenu on screen-size lower than a width of 1024px.
    Allow for overflowing on the vertical scale, not forcing it.

--- src/components/AddSiteModal.js
+++ src/components/AddSiteModal.js
@@ -80,6 +80,12 @@ const AddSiteModal = styled( BaseAddSiteModal )`
 		background-color: rgba(0, 0, 0, 0.6);
 	}
 
+	@media screen and (max-width: 1024px) {
+		&.my-yoast-modal__overlay {
+			bottom: 74px;
+		}
+	}
+
 	&.my-yoast-modal__content {
 		position: absolute;
 		top: 50%;
@@ -87,26 +93,27 @@ const AddSiteModal = styled( BaseAddSiteModal )`
 		right: auto;
 		bottom: auto;
 		width: auto;
+		max-width: 90%;
+		max-height: 90%;
 		border: 0;
 		border-radius: 0;
 		margin-right: -50%;
-		padding: 40px 20px;
+		padding: 2em 1em;
 		transform: translate(-50%, -50%);
 		background-color: #fff;
 		outline: none;
 		
+		@media screen and ( max-width: 800px ) {
+			padding: 1em 0;
+		}
+		
 		@media screen and ( max-width: 500px ) {
-			height: 300px;
-			width: auto;
-			overflow: scroll;
+			overflow-y: auto;
 		}
 		
 		@media screen and ( max-height: 640px ) {
-			height: 300px;
-			width: auto;
-			overflow: scroll;
+			overflow-y: auto;
 		}
-
 	}
 
 	.my-yoast-modal__actions {
