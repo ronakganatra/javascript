e0ca035f41bd99457a8ba498ba43829f47b83a7b
IreneStr <irene@yoast.com>
2017-04-24 14:56:08 +0200

    Add headers and buttons

--- src/components/SubscriptionDetails.js
+++ src/components/SubscriptionDetails.js
@@ -2,21 +2,37 @@ import React from "react";
 import styled from "styled-components";
 import colors from "yoast-components/style-guide/colors.json";
 import { ColumnText, Row, Table, Zebra, Column } from "./Tables";
-import { injectIntl, intlShape, FormattedDate } from "react-intl";
+import { injectIntl, intlShape, FormattedDate, FormattedMessage } from "react-intl";
+import MediaQuery from "react-responsive";
+
+import { LargeButton } from "./Button";
+
+let columnMargin = "10px";
+let responsiveWidthThreshold = 1355;
 
 const SubscriptionDetailsContainer = styled.div`
-	div:nth-child(even) {
-		padding-left: 10px;
-	}
-	div:nth-child(odd) {
-		padding-right: 10px;
-	}
+	display: flex;
+	flex-wrap: wrap;
+	flex-direction: column;
 	background-color: ${ colors.$color_white };
+	box-shadow: 0 2px 8px 0 rgba(0,0,0,0.3);
+	width: 100%;
+	height: 700px;
+	div:nth-child(-n+2) {
+		margin-right: ${ columnMargin };
+	}
+	div:nth-child(3) {
+		margin-left: ${ columnMargin };
+	}
 `;
 
 const ColumnContainer = styled.div`
-	width: 50%
+	width: calc( 50% - ${ columnMargin } );
 	float: left;
+`;
+
+const listHeader = styled.h1`
+	font-size: 1.8em;
 `;
 
 /**
@@ -32,51 +48,85 @@ function SubscriptionDetails( props ) {
 	return (
 		<SubscriptionDetailsContainer>
 			<ColumnContainer>
-				<Table role="list" >
-					<Zebra>
-						<Row>
-							<ColumnText>{ "Start Date" }</ColumnText>
-							<ColumnText> <FormattedDate value={props.startDate} /> </ColumnText>
-						</Row>
-						<Row>
-							<ColumnText>{ "Next Billing" }</ColumnText>
-							<ColumnText> <FormattedDate value={props.nextBilling} /> </ColumnText>
-						</Row>
-					</Zebra>
+				<listHeader>
+					<FormattedMessage id="subscriptions.payment-details.header" defaultMessage="Payment details" />
+				</listHeader>
+				<Table headings={false} role="list" >
+						<Zebra>
+							<Row>
+								<ColumnText>{ "Start Date" }</ColumnText>
+								<ColumnText> <FormattedDate value={props.startDate} /> </ColumnText>
+							</Row>
+							<Row>
+								<ColumnText>{ "Next Billing" }</ColumnText>
+								<ColumnText> <FormattedDate value={props.nextBilling} /> </ColumnText>
+							</Row>
+						</Zebra>
 				</Table>
 			</ColumnContainer>
 			<ColumnContainer>
-				<Table  role="list" >
+				<listHeader>
+					<FormattedMessage id="subscriptions.invoices.header" defaultMessage="Invoices" />
+				</listHeader>
+				<Table headings={false} role="list" >
 					<Zebra>
 						{ props.invoices.map( ( invoice ) => {
 							return <Row { ...invoice } key={ invoice.invoiceId }>
 								<ColumnText><FormattedDate value={invoice.invoiceDate} /></ColumnText>
 								<ColumnText>{ invoice.invoiceCurrency }{ invoice.invoiceAmount }</ColumnText>
-								<ColumnText>{ props.onInvoiceDownload }</ColumnText>
+								<Column>
+									<MediaQuery query={ "(min-width: " + ( responsiveWidthThreshold + 1 ) + "px)" }>
+										<LargeButton onClick={ props.onInvoiceDownload }><FormattedMessage
+											id="subscriptions.buttons.download-invoice" defaultMessage="Invoice" />
+										</LargeButton>
+									</MediaQuery>
+								</Column>
 							</Row>;
 						} ) }
 					</Zebra>
 				</Table>
 			</ColumnContainer>
 			<ColumnContainer>
-				<Table role="list" >
-					<Zebra>
-						<Row>
-							<ColumnText>{ "You can add {} {} more sites to this subscription" }</ColumnText>
-							<Column textAlign="right">
-							</Column>
-						</Row>
-						<Row>
-							<ColumnText>{ "Change subscription level" }</ColumnText>
-							<Column textAlign="right">
-							</Column>
-						</Row>
-						<Row>
-							<ColumnText>{ "Cancel subscription" }</ColumnText>
-							<Column textAlign="right">
-							</Column>
-						</Row>
-					</Zebra>
+				<listHeader>
+					<FormattedMessage id="subscriptions.subscription-details.header" defaultMessage="Subscription details" />
+				</listHeader>
+				<Table headings={false} role="list" >
+						<Zebra>
+							<Row>
+								<ColumnText>
+									<FormattedMessage id="subscriptionss.site-subscriptions-remaining"
+															  defaultMessage={" You can add { howMany } more sites to this subscription."}
+															  values={ { howMany: ( props.max - props.current ) } }/>
+								</ColumnText>
+								<Column>
+									<MediaQuery query={ "(min-width: " + ( responsiveWidthThreshold + 1 ) + "px)" }>
+										<LargeButton onClick={ props.onAddSite }><FormattedMessage
+											id="subscriptions.buttons.add-site" defaultMessage="Add site" />
+										</LargeButton>
+									</MediaQuery>
+								</Column>
+							</Row>
+							<Row>
+								<ColumnText>{ "Change subscription level" }</ColumnText>
+								<Column>
+									<MediaQuery query={ "(min-width: " + ( responsiveWidthThreshold + 1 ) + "px)" }>
+										<LargeButton onClick={ props.onShop }><FormattedMessage
+											id="subscriptions.buttons.shop" defaultMessage="Shop" />
+										</LargeButton>
+									</MediaQuery>
+								</Column>
+							</Row>
+							<Row>
+								<ColumnText>{ "Cancel subscription" }</ColumnText>
+								<Column>
+									<MediaQuery query={ "(min-width: " + ( responsiveWidthThreshold + 1 ) + "px)" }>
+										<LargeButton onClick={ props.onCancel }><FormattedMessage
+											id="subscriptions.buttons.cancel" defaultMessage="Cancel" />
+										</LargeButton>
+									</MediaQuery>
+								</Column>
+							</Row>
+						</Zebra>
 				</Table>
 			</ColumnContainer>
 		</SubscriptionDetailsContainer>
@@ -87,6 +137,8 @@ SubscriptionDetails.propTypes = {
 	startDate: React.PropTypes.instanceOf( Date ).isRequired,
 	nextBilling: React.PropTypes.instanceOf( Date ).isRequired,
 	invoices: React.PropTypes.array,
+	max: React.PropTypes.number.isRequired,
+	current: React.PropTypes.number.isRequired,
 	onAddSite: React.PropTypes.func.isRequired,
 	onShop: React.PropTypes.func.isRequired,
 	onCancel: React.PropTypes.func.isRequired,
