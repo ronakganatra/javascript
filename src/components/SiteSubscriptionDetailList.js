5f424b77976cfb1c8769e56a4fe66ca880d884b7
boblinthorst <bob.linthorst@gmail.com>
2017-04-25 15:53:51 +0200

    Changed name of prop

--- src/components/SiteSubscriptionDetailList.js
+++ src/components/SiteSubscriptionDetailList.js
@@ -27,7 +27,7 @@ function SiteSubscriptionDetailList( props ) {
 						return <SiteSubscriptionDetail
 							{ ...subscription }
 							key={ subscription.id }
-							id={ subscription.productId }
+							name={ subscription.productId }
 							onAddMoreSlotsClick={ props.onAddMoreSlotsClick }
 							onMoreInfoClick={ props.onMoreInfoClick }
 							onSettingsClick={ props.onSettingsClick }
