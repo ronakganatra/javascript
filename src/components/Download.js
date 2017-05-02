10bb2e3448397a9dbe014c5e002110a7c365100a
IreneStr <irene@yoast.com>
2017-05-02 09:19:04 +0200

    Fix alignment, add responsive view and test

--- src/components/Download.js
+++ src/components/Download.js
@@ -12,45 +12,64 @@ const messages = defineMessages( {
 	},
 } );
 
+let responsiveWidthThreshold = 1355;
+let mobileViewThreshold = 600;
+
 const DownloadContainer = styled.div`
-	width: calc( 25% - 10px - 10px );
-	margin: 10px 10px 10px 10px;
-	background-color: ${ colors.$color_pink_light };
+	margin: 8px;
+	padding: 16px;
+	background-color: ${ colors.$color_grey_light };
 	display: flex;
 	flex-direction: column;
-	text-align: center;
 	align-items: center;
+	
+	@media screen and ( min-width: ${ responsiveWidthThreshold }px ) {
+		width: calc( 25% - 8px - 8px );
+	}
+
+	@media screen and ( max-width: ${ responsiveWidthThreshold }px ) {
+		width: calc( 50% - 8px - 8px );
+	}
+	
+	@media screen and ( max-width: ${ mobileViewThreshold }px ) {
+		width: calc( 100% - 8px - 8px );
+	}
 `;
 
 const DownloadIcon = styled.img`
-	width: 90px;
-	height: 90px;
+	width: 112px;
+	height: 112px;
+	padding: 0 0 5px;
+`;
+
+const DownloadList = styled.ul`
+	padding: 0;
+	margin: 0px 0 0;
 `;
 
 const DownloadListItem = styled.li`
 	list-style: none;
 	text-align: center;
+	margin: 20px 0 0;
 `;
 
-const DownloadList = function( props ) {
-	return (
-		<ul>
-			{ props.buttons.map( button => {
-				return ( <DownloadListItem> <IconButton iconSource={ downloadIcon }
-										  onClick={ button.onButtonClick }
-										  iconSize={ "16px" }>
-					{ props.intl.formatMessage( messages.downloadButton ) }
-				</IconButton>
-					{ button.label }</DownloadListItem> );
-			} )	}
-		</ul>
-	);
-};
+const ProductName = styled.h2`
+	font-size: 1.5em;
+	font-weight: 700px;
+	margin: 0;
+`;
 
-DownloadList.propTypes = {
-	buttons: React.PropTypes.array.isRequired,
-	intl: intlShape.isRequired,
-};
+const ProductVersion = styled.p`
+	font-size: 1em;
+	font-weight: 300px;
+	margin: 0 0 16px;
+`;
+
+const ButtonLabel = styled.p`
+	font-size: 0.9em;
+	font-weight: 300px;
+	margin: 2px 0 12px;
+`;
 
 /**
  * The Download component.
@@ -62,10 +81,21 @@ DownloadList.propTypes = {
 function Download( props ) {
 	return (
 		<DownloadContainer>
-			<span>{ props.product }</span>
-			<span>{ props.version }</span>
-			<DownloadIcon src={ props.iconSource }/>
-			<DownloadList buttons={ props.buttons } intl={ props.intl } />
+			<ProductName>{ props.product }</ProductName>
+			<ProductVersion>{ props.version }</ProductVersion>
+			<DownloadIcon src={ props.iconSource } alt=""/>
+			<DownloadList>
+				{ props.buttons.map( button => {
+					return ( <DownloadListItem key={ button.label }> <IconButton
+															iconSource={ downloadIcon }
+															onClick={ button.onButtonClick }
+															iconSize={ "16px" }
+					>
+						{ props.intl.formatMessage( messages.downloadButton ) }
+					</IconButton>
+						<ButtonLabel>{ button.label }</ButtonLabel></DownloadListItem> );
+				} )	}
+			</DownloadList>
 		</DownloadContainer>
 	);
 }
