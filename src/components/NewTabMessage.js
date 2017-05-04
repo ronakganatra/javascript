b568b9cb9eba503336740d50d2bb98941c013155
Andrea Fercia <a.fercia@gmail.com>
2017-05-04 16:39:47 +0200

    Process CR feedback and update snapshots.

--- src/components/NewTabMessage.js
+++ src/components/NewTabMessage.js
@@ -2,14 +2,14 @@ import React from "react";
 import { FormattedMessage } from "react-intl";
 
 /**
- * Returns the rendered Account Page component.
+ * Returns the rendered Opens in a New Tab Message component.
  *
- * @returns {ReactElement} The rendered account page.
+ * @returns {ReactElement} The rendered NewTabMessage component.
  */
 export default function NewTabMessage() {
 	return (
 		<span className="screen-reader-text">
-			<FormattedMessage id="link.new-window" defaultMessage="(opens in a new browser's tab)" />
+			<FormattedMessage id="link.new-window" defaultMessage="(Opens in a new browser tab)" />
 		</span>
 	);
 }
