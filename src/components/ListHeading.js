0795ef7d925e37e6aeb09e386ba4cc8453287237
IreneStr <irene@yoast.com>
2017-05-04 12:02:49 +0200

    Make ListHeading into a separate component

---
+++ src/components/ListHeading.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+import colors from "yoast-components/style-guide/colors.json";
+
+export const ListHeading = styled.h2`
+	font-size: 1em;
+	font-weight: 400; 
+	padding: 20px 0 20px 30px;
+	margin: 0;
+	border-bottom: 1px solid ${ colors.$color_grey_medium };
+`;
