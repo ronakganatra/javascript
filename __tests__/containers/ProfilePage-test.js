6895a10a944e971ab0692638a646269b77fa4b34
omarreiss <omar.reiss@gmail.com>
2017-05-06 11:26:56 +0200

    Add unit tests for profile page container

---
+++ __tests__/containers/ProfilePage-test.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { mapStateToProps, mapDispatchToProps } from "../../src/containers/ProfilePage"
+import { profileUpdateEmail } from "../../src/actions/user";
+import { url } from "gravatar";
+let avatarPlaceholder = "https://s3.amazonaws.com/yoast-my-yoast/My_Yoast_default_avatar.png";
+
+test('the mapStateToProps function', () => {
+	let state = {
+		user: {
+			data: {
+				profile: {
+					email: "test@test.test"
+				},
+			},
+		},
+	};
+
+	let expected = {
+		email: "test@test.test",
+		image: url( state.user.data.profile.email, {
+			s: "150",
+			r: "pg",
+			d: avatarPlaceholder,
+			protocol: "https",
+		} ),
+	};
+
+	expect( mapStateToProps( state ) ).toEqual( expected );
+} );
+
+test('the mapDispatchToProps function to call profileUpdateEmail action with onChangeEmail when confirm is true', () => {
+	const dispatch = jest.fn();
+
+	let props = mapDispatchToProps( dispatch );
+
+	props.onUpdateEmail( "test@test.test" );
+
+	expect( dispatch ).toHaveBeenCalledWith( profileUpdateEmail( "test@test.test" ) );
+} );
