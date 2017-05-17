cceaf0af98b75968f07ba75567a5266addf21612
Anton Timmermans <email@atimmer.com>
2017-05-17 15:44:02 +0200

    Remove console.log

--- src/containers/OrdersPage.js
+++ src/containers/OrdersPage.js
@@ -22,7 +22,6 @@ export const mapStateToProps = ( state ) => {
 
 	let query = state.ui.search.query;
 	if ( query.length > 0 ) {
-		console.log( orders );
 		orders = orders.filter( ( order ) => {
 			return order.items.find( item => item.productName.toUpperCase().includes( query.toUpperCase() ) ) ||
 				order.orderNumber.toUpperCase().includes( query.toUpperCase() );
