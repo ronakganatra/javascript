957a539bf0faa5d0ada81b46948fd04f67b9ddfc
Andrea Fercia <a.fercia@gmail.com>
2017-05-23 15:08:53 +0200

    Add skip link.

--- src/App.js
+++ src/App.js
@@ -10,9 +10,10 @@ import { Provider } from "react-redux";
 import { injectGlobal } from "styled-components";
 import { ConnectedRouter } from "react-router-redux";
 import colors from "yoast-components/style-guide/colors.json";
-import { IntlProvider } from "react-intl";
+import { IntlProvider, FormattedMessage } from "react-intl";
 import DebugInfo from "./components/DebugInfo";
 import { Logo } from "./components/Logo";
+import SkipLink from "./components/SkipLink";
 
 /*
  * Helper method to write global CSS.
@@ -37,6 +38,9 @@ class App extends Component {
 					<ConnectedRouter history={ this.props.history }>
 						<Layout>
 							<Sidebar>
+								<SkipLink>
+									<FormattedMessage id="skiplink" defaultMessage={ "Skip to main content" } />
+								</SkipLink>
 								<header role="banner">
 									<Logo size="200px" />
 								</header>
