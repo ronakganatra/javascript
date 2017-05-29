1c73384ae0ff1d3e85cb9b18c7b1ae1843cac378
Maarten Leenders <maarten@yoast.com>
2017-05-29 09:07:48 +0200

    update test because of button change to link

--- __tests__/components/SubscriptionHeader-test.js
+++ __tests__/components/SubscriptionHeader-test.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import SubscriptionHeader from "../../src/components/SubscriptionHeader";
+import { MemoryRouter } from 'react-router-dom';
 
 test('the subscription header matches the snapshot', () => {
 	const component = renderer.create(
-		<SubscriptionHeader name="Subscription" />
+		<MemoryRouter>
+			<SubscriptionHeader name="Subscription" />
+		</MemoryRouter>
 	);
 
 	let tree = component.toJSON();
