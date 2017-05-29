058ffab04af5f01b822349ceb05bd9f8b366736b
Thijs Hulshof <thijsh@yoast.com>
2017-05-29 14:57:34 +0200

    Fixed tests

--- __tests__/containers/SitePage-test.js
+++ __tests__/containers/SitePage-test.js
@@ -161,6 +161,8 @@ test('the mapStateToProps function', () => {
 				used: 0,
 				limit: 0,
 				isEnabled: false,
+				isAvailable: false,
+
 				subscriptionId: "",
 			},
 			{
@@ -173,6 +175,7 @@ test('the mapStateToProps function', () => {
 				used: 0,
 				limit: 0,
 				isEnabled: false,
+				isAvailable: false,
 				subscriptionId: "",
 			},
 		],
