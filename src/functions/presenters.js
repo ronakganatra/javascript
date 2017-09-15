264defd895ab3e4cbda3dd6a29e137df2c265bc2
Herre Groen <herre@yoast.com>
2017-09-15 11:34:11 +0200

    Add order status and fix currency presenters.

--- src/functions/presenters.js
+++ src/functions/presenters.js
@@ -1,23 +1,51 @@
+/**
+ * Pads a string or number with zeroes, for use with currencies.
+ *
+ * @param {string|number} value
+ *
+ * @returns {string}
+ */
 function zeroPad( value ) {
 	return value.toString().padStart( 2, '0' );
 }
 
+/**
+ * Presents a numerical value as a euro currency string.
+ *
+ * @param {number} value The amount in cents.
+ *
+ * @returns {string} A currency string.
+ */
 export function euroPresenter( value ) {
 	if ( value === null ) {
 		return;
 	}
 
-	return `€ ${ zeroPad( value / 100 ) },${ zeroPad( value % 100 ) }`;
+	return `€ ${ Math.floor( value / 100 ) },${ zeroPad( value % 100 ) }`;
 }
 
+/**
+ * Presents a numerical value as a dollar currency string.
+ *
+ * @param {number} value The amount in cents.
+ *
+ * @returns {string} A currency string.
+ */
 export function dollarPresenter( value ) {
 	if ( value === null ) {
 		return;
 	}
 
-	return `$ ${ zeroPad( value / 100 ) },${ zeroPad( value % 100 ) }`;
+	return `$ ${ Math.floor( value / 100 ) },${ zeroPad( value % 100 ) }`;
 }
 
+/**
+ * Presents a value that can be turned into a Date as that date's localeString.
+ *
+ * @param {string|number|Date} value The date, passed to the Date constructor.
+ *
+ * @returns {string} That date's localeString.
+ */
 export function datePresenter( value ) {
 	if ( value === null ) {
 		return;
