664e882c69d4e6088eedf11bcbcf07ea880aeb4f
Thijs Hulshof <thijsh@yoast.com>
2017-11-20 08:46:37 +0100

    Fix tests

--- __tests__/components/CoursesPage-test.js
+++ __tests__/components/CoursesPage-test.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import CoursesPage from '../../src/components/CoursesPage';
+import { MemoryRouter } from "react-router-dom";
 import { createComponentWithIntl } from "../../utils";
 
 test('the courses page component matches the snapshot', () => {
 	const component = createComponentWithIntl(
-		<CoursesPage loadCourses={ () => { "dispatch action retrieveCourses" } } loadCoursesEnrollments={ () => { "dispatch action retrieveCoursesEnrollments" } } />
-	);
+		<MemoryRouter>
+			<CoursesPage loadCourses={ () => { "dispatch action retrieveCourses" } } loadCoursesEnrollments={ () => { "dispatch action retrieveCoursesEnrollments" } } />
+		</MemoryRouter>
+		);
 
 	let tree = component.toJSON();
 	expect(tree).toMatchSnapshot();
