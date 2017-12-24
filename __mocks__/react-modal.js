c8a84e6714038c4b1a9967ddb3993b09093c4fa1
Maarten Leenders <maarten@yoast.com>
2017-12-24 10:14:21 +0100

    fix initial issue with a createClass file

--- __mocks__/react-modal.js
+++ __mocks__/react-modal.js
@@ -1,23 +1,12 @@
 import React from "react";
-import PropTypes from "prop-types";
 
+export default class MockedModal extends React.Component {
 
-class Modal extends React.Component {
-	render(){
+	static setAppElement() {
+		return jest.fn();
+	}
+
+	render() {
 		return null;
 	}
 }
-
-Modal.propTypes = {
-	displayName: PropTypes.string,
-	statics: PropTypes.Object,
-};
-
-Modal.defaultProps = {
-	displayName: 'MockedModal',
-	statics: {
-		setAppElement: jest.fn(),
-	},
-};
-
-module.exports = Modal;
