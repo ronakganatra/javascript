3a0b366841640cecb4ac2f9b28a87408a759cdc8
Maarten Leenders <maarten@yoast.com>
2018-03-07 16:59:54 +0100

    implement first stages of stepper in modal

--- src/components/sites/ConfigurationRequest.js
+++ src/components/sites/ConfigurationRequest.js
@@ -4,19 +4,17 @@ import PropTypes from "prop-types";
 import { LargeButton, makeButtonFullWidth, LargeSecondaryButton } from "../Button.js";
 import styled from "styled-components";
 import { ModalHeading } from "../Headings";
+import { Stepper } from "material-ui/Stepper";
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+import { FlatButton, RaisedButton, Step, StepButton, StepContent, StepLabel } from "material-ui";
+import { BottomRightButtons } from "../PaperStyles";
 
 const ConfigurationRequestModal = styled.div`
-	max-width: 640px;
+	width: 640px;
+	min-width: 90%;
 	margin: auto;
 	font-weight: 300;
 	font-size: 1em;
-
-	label {
-		display: inline-block;
-		font-weight: 300;
-		font-size: 1em;
-		margin: 16px 0 8px;
-	}
 `;
 
 const WideLargeButton = makeButtonFullWidth( LargeButton );
@@ -34,8 +32,48 @@ class ConfigurationRequest extends React.Component {
 		super( props );
 
 		this.state = {
-			configStep: 1,
+			stepIndex: 0,
 		};
+	}
+
+	handleNext() {
+		const { stepIndex } = this.state;
+		this.setState( {
+			stepIndex: stepIndex + 1,
+		} );
+	}
+
+	handlePrev() {
+		const { stepIndex } = this.state;
+		if ( stepIndex > 0 ) {
+			this.setState( { stepIndex: stepIndex - 1 } );
+		}
+	}
+
+	renderStepActions( step ) {
+		const { stepIndex } = this.state;
+
+		return (
+			<div style={ { margin: "12px 0" } }>
+				{ step > 0 && (
+					<FlatButton
+						label="Back"
+						disabled={ stepIndex === 0 }
+						disableTouchRipple={ true }
+						disableFocusRipple={ true }
+						onClick={ this.handlePrev.bind( this ) }
+						style={ { marginRight: 12 } }
+					/>
+				) }
+				<RaisedButton
+					label={ stepIndex === 3 ? "Finish" : "Next" }
+					disableTouchRipple={ true }
+					disableFocusRipple={ true }
+					primary={ true }
+					onClick={ this.handleNext.bind( this ) }
+				/>
+			</div>
+		);
 	}
 
 	/**
@@ -44,28 +82,110 @@ class ConfigurationRequest extends React.Component {
 	 * @returns {ReactElement} The rendered html.
 	 */
 	render() {
+		const { stepIndex } = this.state;
+
 		return (
-			<ConfigurationRequestModal>
-				<ModalHeading>
-					<FormattedMessage
-						id="configuration.modal.heading"
-						defaultMessage="Configuration service ({configStep}/4)"
-						values={ { configStep: this.state.configStep } }
-					/>
-				</ModalHeading>
-				<div>
-					<p>Stapje 1</p>
-					<p>Stapje 2</p>
-					<p>Stapje 3</p>
-					<p>Stapje 4</p>
-				</div>
-				<WideSecondaryButton onClick={ this.props.onClose } >
-					Secondary Butt
-				</WideSecondaryButton>
-				<WideLargeButton onClick={ () => console.log( "primary click" ) }>
-					Primary butt
-				</WideLargeButton>
-			</ConfigurationRequestModal>
+			<MuiThemeProvider>
+				<ConfigurationRequestModal>
+					<ModalHeading>
+						<FormattedMessage
+							id="configuration.modal.heading"
+							defaultMessage="Configuration service ({configStep}/4)"
+							values={ { configStep: this.state.stepIndex+1 } }
+						/>
+					</ModalHeading>
+					<Stepper activeStep={ stepIndex } linear={ false } orientation={ "vertical" }>
+						<Step>
+							<StepButton
+								onClick={ () => this.setState( { stepIndex: 0 } ) }
+								disableFocusRipple={ true }
+								disableTouchRipple={ true }
+							>
+								<StepLabel>
+									<FormattedMessage
+										id="configuration.modal.step-one"
+										defaultMessage="Administrator login"
+									/>
+								</StepLabel>
+							</StepButton>
+							<StepContent>
+								<p>
+									Try out different ad text to see what brings in the most customers,
+									and learn how to enhance your ads using features like ad extensions.
+									If you run into any problems with your ads, find out how to tell if
+									they're running and how to resolve approval issues.
+								</p>
+								{ this.renderStepActions( 0 ) }
+							</StepContent>
+						</Step>
+						<Step>
+							<StepButton onClick={ () => this.setState( { stepIndex: 1 } ) }>
+								<StepLabel>
+									<FormattedMessage
+										id="configuration.modal.step-two"
+										defaultMessage="Backup"
+									/>
+								</StepLabel>
+							</StepButton>
+							<StepContent>
+								<p>
+									Try out different ad text to see what brings in the most customers,
+									and learn how to enhance your ads using features like ad extensions.
+									If you run into any problems with your ads, find out how to tell if
+									they're running and how to resolve approval issues.
+								</p>
+								{ this.renderStepActions( 1 ) }
+							</StepContent>
+						</Step>
+						<Step>
+							<StepButton onClick={ () => this.setState( { stepIndex: 2 } ) }>
+								<StepLabel>
+									<FormattedMessage
+										id="configuration.modal.step-three"
+										defaultMessage="Import data from another SEO plugin"
+									/>
+								</StepLabel>
+							</StepButton>
+							<StepContent>
+								<p>
+									Try out different ad text to see what brings in the most customers,
+									and learn how to enhance your ads using features like ad extensions.
+									If you run into any problems with your ads, find out how to tell if
+									they're running and how to resolve approval issues.
+								</p>
+								{ this.renderStepActions( 2 ) }
+							</StepContent>
+						</Step>
+						<Step>
+							<StepButton onClick={ () => this.setState( { stepIndex: 3 } ) }>
+								<StepLabel>
+									<FormattedMessage
+										id="configuration.modal.step-four"
+										defaultMessage="Google Search Console"
+									/>
+								</StepLabel>
+							</StepButton>
+							<StepContent>
+								<p>
+									Try out different ad text to see what brings in the most customers,
+									and learn how to enhance your ads using features like ad extensions.
+									If you run into any problems with your ads, find out how to tell if
+									they're running and how to resolve approval issues.
+								</p>
+								{ this.renderStepActions( 3 ) }
+							</StepContent>
+						</Step>
+					</Stepper>
+					<BottomRightButtons>
+						<WideSecondaryButton onClick={ this.props.onClose } >
+							Secondary Butt
+						</WideSecondaryButton>
+						<WideLargeButton onClick={ () => console.log( "primary click" ) }>
+							Primary butt
+						</WideLargeButton>
+					</BottomRightButtons>
+				</ConfigurationRequestModal>
+			</MuiThemeProvider>
 		);
 	}
 }
