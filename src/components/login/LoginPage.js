9d3fe64fcf90ccf6fe069e360c0b2ca8fcb2197c
Hans-Christiaan <hans-christiaan@yoast.com>
2018-05-23 16:47:39 +0200

    Worked on one and two column login layout.

--- src/components/login/LoginPage.js
+++ src/components/login/LoginPage.js
@@ -1,5 +1,7 @@
 import React from "react";
+import PropTypes from "prop-types";
 import { defineMessages, injectIntl } from "react-intl";
+import styled from "styled-components";
 
 // Components.
 import LoginMessagePaper from "./LoginMessagePaper";
@@ -14,6 +16,20 @@ const messages = defineMessages( {
 		defaultMessage: "Log in with your email address and password. If you don't remember your password, just reset it!",
 	},
 } );
+
+const ColumnLayout = styled.div`
+
+`;
+
+const ColumnLeft = styled.div`
+	float: left;
+	width: 480px;
+`;
+
+const ColumnRight = styled.div`
+	/* 480 + 48px */
+	margin-left: 528px;
+`;
 
 /**
  * A function that returns the Courses Page component.
@@ -30,12 +46,29 @@ class LoginPage extends React.Component {
 		super();
 	}
 
-	render() {
-		return (
+	getRightColumn() {
+		return <ColumnRight>
 			<LoginMessagePaper header={ messages.header } message={ messages.message }/>
+		</ColumnRight>;
+	}
+
+	render() {
+		let twoColumns = this.props.children > 1;
+
+		return (
+			<ColumnLayout>
+				<ColumnLeft>
+					<LoginMessagePaper header={ messages.header } message={ messages.message }/>
+				</ColumnLeft>
+				{ twoColumns ? this.getRightColumn() : null }
+			</ColumnLayout>
 		);
 	}
 }
+
+LoginPage.propTypes = {
+	children: PropTypes.array,
+};
 
 export default injectIntl( LoginPage );
 
