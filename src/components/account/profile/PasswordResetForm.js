d1a012ea22d61e4780f7557b6746de3b21b8572b
Hans-Christiaan <hans-christiaan@yoast.com>
2018-06-07 16:25:18 +0200

    Fixed linting warnings.

--- src/components/account/profile/PasswordResetForm.js
+++ src/components/account/profile/PasswordResetForm.js
@@ -34,7 +34,6 @@ const TextInput = styled( InputField )`
 	background-color: ${ colors.$color_background_light };
 `;
 
-
 /**
  * Returns the rendered PasswordResetForm component.
  *
@@ -78,9 +77,11 @@ class PasswordResetForm extends React.Component {
 	onCurrentPassword( event ) {
 		this.setState( { currentPassword: event.target.value } );
 	}
+
 	onNewPassword( event ) {
 		this.setState( { newPassword: event.target.value } );
 	}
+
 	onConfirmPassword( event ) {
 		this.setState( { confirmPassword: event.target.value } );
 	}
@@ -115,7 +116,7 @@ class PasswordResetForm extends React.Component {
 		 * While saving: prevent multiple submissions but don't disable the
 		 * button for better accessibility (avoid keyboard focus loss).
 		 */
-		/*
+		/* Should be enabled later:
 		 if ( this.props.isSaving ) {
 		 return;
 		 }
@@ -125,7 +126,6 @@ class PasswordResetForm extends React.Component {
 		 */
 		// Should be updated when encryption is developed.
 		this.setState( { onDiscard: false } );
-		console.log( "handle sumbit should be updated" );
 	}
 
 	componentDidUpdate() {
@@ -142,54 +142,54 @@ class PasswordResetForm extends React.Component {
 	 */
 	render() {
 		return (
-				<FormGroup onSubmit={ this.handleSubmit }>
-					<StyledLabel htmlFor="current-password">
-						<FormattedMessage
-							id={messages.currentPassword.id}
-							defaultMessage={messages.currentPassword.defaultMessage}
-						/>
-					</StyledLabel>
-					<TextInput
-						width="100%"
-						margin-right="5px"
-						id="current-password"
-						name="current password"
-						type="text"
-						value={ this.state.currentPassword }
-						onChange={ this.onCurrentPassword }
+			<FormGroup onSubmit={ this.handleSubmit }>
+				<StyledLabel htmlFor="current-password">
+					<FormattedMessage
+						id={ messages.currentPassword.id }
+						defaultMessage={ messages.currentPassword.defaultMessage }
 					/>
-					<StyledLabel htmlFor="new-password">
-						<FormattedMessage
-							id={messages.newPassword.id}
-							defaultMessage={messages.newPassword.defaultMessage}
-						/>
-					</StyledLabel>
-					<TextInput
-						width="100%"
-						margin-right="5px"
-						id="new-password"
-						name="new password"
-						type="text"
-						value={ this.state.newPassword }
-						onChange={ this.onNewPassword }
+				</StyledLabel>
+				<TextInput
+					width="100%"
+					margin-right="5px"
+					id="current-password"
+					name="current password"
+					type="text"
+					value={ this.state.currentPassword }
+					onChange={ this.onCurrentPassword }
+				/>
+				<StyledLabel htmlFor="new-password">
+					<FormattedMessage
+						id={ messages.newPassword.id }
+						defaultMessage={ messages.newPassword.defaultMessage }
 					/>
-					<StyledLabel htmlFor="confirm-password">
-						<FormattedMessage
-							id={messages.confirmPassword.id}
-							defaultMessage={messages.confirmPassword.defaultMessage}
-						/>
-					</StyledLabel>
-					<TextInput
-						width="100%"
-						margin-right="5px"
-						id="confirm-password"
-						name="confirm password"
-						type="text"
-						value={ this.state.confirmPassword }
-						onChange={ this.onConfirmPassword }
+				</StyledLabel>
+				<TextInput
+					width="100%"
+					margin-right="5px"
+					id="new-password"
+					name="new password"
+					type="text"
+					value={ this.state.newPassword }
+					onChange={ this.onNewPassword }
+				/>
+				<StyledLabel htmlFor="confirm-password">
+					<FormattedMessage
+						id={ messages.confirmPassword.id }
+						defaultMessage={ messages.confirmPassword.defaultMessage }
 					/>
-					{ getChangeButtons( "password", this.props.intl, this.props.isSavingPassword, this.props.passwordIsSaved, this.discardChanges ) }
-				</FormGroup>
+				</StyledLabel>
+				<TextInput
+					width="100%"
+					margin-right="5px"
+					id="confirm-password"
+					name="confirm password"
+					type="text"
+					value={ this.state.confirmPassword }
+					onChange={ this.onConfirmPassword }
+				/>
+				{ getChangeButtons( "password", this.props.intl, this.props.isSavingPassword, this.props.passwordIsSaved, this.discardChanges ) }
+			</FormGroup>
 		);
 	}
 }
