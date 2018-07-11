7b779cd518dd797621c67ef2c11506e764571a03
NicolleY <nicolle@yoast.com>
2018-07-11 16:58:28 +0200

    Edit the attemptResetPassword function let request and added a redirect in .then.

--- src/containers/ResetPassword.js
+++ src/containers/ResetPassword.js
@@ -1,25 +1,33 @@
 import { connect } from "react-redux";
 import ResetPasswordPage from "../components/login/ResetPasswordPage";
 import { doRequest, prepareInternalRequest } from "../functions/api";
+import { getUserId } from "../functions/auth";
+import getEnv from "../functions/getEnv";
 
 export const mapDispatchToProps = ( dispatch, ownProps ) => {
 	return {
 		attemptResetPassword: ( data ) => {
-			 let params = data;
-			 let request = prepareInternalRequest( "Customers/resetPassword/", "PATCH", params );
-			 doRequest( request )
-			 .then( () => {
-			 console.log( "succceeded!" );
-			 }
-			 )
-			 .catch( ( error ) => {
+			let params = data;
+			let userId = getUserId();
+			let request = prepareInternalRequest( `Customers/${userId}/resetPassword/`, "PATCH", params );
+			doRequest( request )
+			.then( () => {
+				document.location.href = getEnv( "LOGIN_URL", "http://my.yoast.test:3001/login" );
+			}
+			)
+			.catch( ( error ) => {
 				return error;
-			 } );
+			} );
 		},
 	};
 };
 
+export const mapStateToProps = ( state ) => {
+	return state.router;
+};
+
 const ResetPasswordContainer = connect(
+	mapStateToProps,
 	mapDispatchToProps
 )( ResetPasswordPage );
 
