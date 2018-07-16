4bb0def4992067286c842ae30e4ba5e9636b4f65
NicolleY <nicolle@yoast.com>
2018-07-16 13:18:31 +0200

    Removed id from path in customer.js and removed userId from internal request

--- src/containers/ResetPasswordEmail.js
+++ src/containers/ResetPasswordEmail.js
@@ -1,15 +1,13 @@
 import { connect } from "react-redux";
 import ResetPasswordEmailPage from "../components/login/ResetPasswordEmailPage";
 import { doRequest, prepareInternalRequest } from "../functions/api";
-import { getUserId } from "../functions/auth";
 import getEnv from "../functions/getEnv";
 
 export const mapDispatchToProps = ( dispatch, ownProps ) => {
 	return {
 		attemptResetPasswordEmail: ( data ) => {
 			let params = data;
-			let userId = getUserId();
-			let request = prepareInternalRequest( `Customers/${userId}/sendResetPasswordEmail/`, "POST", params );
+			let request = prepareInternalRequest( "Customers/sendResetPasswordEmail/", "POST", params );
 			doRequest( request )
 			.then( () => {
 				document.location.href = getEnv( "LOGIN_URL", "http://my.yoast.test:3001/emailSuccess" );
