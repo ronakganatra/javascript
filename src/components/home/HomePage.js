072ca04e6463294499a71c841bc20d850be554ee
Maarten Leenders <maarten@yoast.com>
2018-09-19 10:09:12 +0200

    Create placeholder homepage and container

---
+++ src/components/home/HomePage.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { injectIntl, intlShape, defineMessages } from "react-intl";
+import { speak } from "@wordpress/a11y";
+
+
+const messages = defineMessages( {
+	homePageLoaded: {
+		id: "home.page.loaded",
+		defaultMessage: "Home page loaded",
+	},
+} );
+
+/**
+ * Returns the rendered Downloads Page component.
+ *
+ * @param {Object} props The props to use.
+ *
+ * @returns {ReactElement} The rendered downloads page.
+ */
+class HomePage extends React.Component {
+	componentDidMount() {
+		// Announce navigation to assistive technologies.
+		let message = this.props.intl.formatMessage( messages.homePageLoaded );
+		speak( message );
+	}
+
+	render() {
+		return (
+			<div>
+				<h1>WORK IN PROGRESS</h1>
+			</div>
+		);
+	}
+}
+
+export default injectIntl( HomePage );
+
+HomePage.propTypes = {
+	intl: intlShape.isRequired,
+};
+
