1537aefa75ce13876624c386ea85d8aa2031a171
Maarten Leenders <maarten@yoast.com>
2018-09-25 11:58:08 +0200

    Create base UpsellCard and update Card to accept id

--- src/components/home/UpsellCard.js
+++ src/components/home/UpsellCard.js
@@ -4,22 +4,87 @@ import { FormattedMessage, injectIntl } from "react-intl";
 import { FullHeightCard } from "../Card";
 import styled from "styled-components";
 import colors from "yoast-components/style-guide/colors";
-import LocalIcon from "../../icons/Yoast/Local_SEO_Icon_Small.svg";
 
 const Header = styled.h2`
 	padding: 0;
-	margin: 0;
-	margin-bottom: 15px;
+	margin: 16px 0;
 	color: ${ colors.$color_pink_dark };
 	font-weight: 50;
 	font-size: 1.5em;
 	text-decoration: none;
 `;
 
-const Details = styled.div`
-	margin: 24px 0;
+const ListContainer = styled.div`
+	margin: 0;
 	flex-grow: 1;
 `;
+
+const ListItem = styled.div`
+	white-space: nowrap;
+	display: flex;
+	box-sizing: border-box;
+	:not(:last-child) {
+		margin-bottom: 16px;
+	}
+`;
+
+const Block = styled.div`
+	display: inline-block;
+`;
+
+const TextBlock = styled( Block )`
+	margin-left: 8px;
+	vertical-align: top;
+	white-space: normal;
+`;
+
+const Icon = styled.img`
+	width: 64px;
+	height: 64px;
+	display: block;
+`;
+
+const Description = styled.p`
+	margin: 0;
+	white-space: normal;
+	max-width: 100%;
+	line-height: 0.9;
+`;
+
+const UpsellListItem = ( props ) => {
+	return(
+		<ListItem>
+			<Block>
+				<Icon src={ props.icon } />
+			</Block>
+			<TextBlock>
+				<a href={ props.link.url } target="blank" >{ props.link.name }</a>
+				<Description>
+					<FormattedMessage
+						id={ props.description.id }
+						defaultMessage={ props.description.defaultMessage }
+					/>
+				</Description>
+			</TextBlock>
+		</ListItem>
+	);
+};
+
+UpsellListItem.propTypes = {
+	icon: PropTypes.string.isRequired,
+	link: PropTypes.shape(
+		{
+			name: PropTypes.string.isRequired,
+			url: PropTypes.string.isRequired,
+		}
+	),
+	description: PropTypes.shape(
+		{
+			id: PropTypes.string.isRequired,
+			defaultMessage: PropTypes.string.isRequired,
+		}
+	),
+};
 
 /**
  * A function that returns the Courses Progress Tab component.
@@ -32,26 +97,32 @@ const UpsellCard = ( props ) => {
 	return (
 		<FullHeightCard
 			className={ "UpsellCard" }
+			id={ props.id }
 		>
-			<Details>
-				<Header>
-					<FormattedMessage id={ "home.upsellcard.header" } defaultMessage={ props.header } />
-				</Header>
-				<ul>
-					<li>
-						<img src={ LocalIcon } />
-						<p>
-							"stuff"
-						</p>
-					</li>
-				</ul>
-			</Details>
+			<Header>
+				<FormattedMessage id={ `${ props.id }.${ props.header.id }` } defaultMessage={ props.header.defaultMessage } />
+			</Header>
+			<ListContainer>
+				{
+					props.listPropsArray.map( ( listProps, index ) => {
+						return(
+							<UpsellListItem { ...listProps } key={ `${ props.id }-key-${ index }` } />
+						);
+					} )
+				}
+			</ListContainer>
 		</FullHeightCard>
 	);
 };
 
 UpsellCard.propTypes = {
-	header: PropTypes.string.isRequired,
+	id: PropTypes.string,
+	header: PropTypes.object.isRequired,
+	listPropsArray: PropTypes.array.isRequired,
+};
+
+UpsellCard.defaultProps = {
+	id: null,
 };
 
 export default injectIntl( UpsellCard );
