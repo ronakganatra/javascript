9499cd68a35a826a3772bfc6390a5799a8abf7ba
Maarten Leenders <maarten@yoast.com>
2018-10-03 16:52:42 +0200

    Finalize first iteration of SupportCard

--- src/components/home/SupportCard.js
+++ src/components/home/SupportCard.js
@@ -4,11 +4,13 @@ import styled from "styled-components";
 import colors from "yoast-components/style-guide/colors";
 import ForumsImage from "../../images/supportForums.svg";
 import KbImage from "../../images/supportKb.svg";
+import Link from "../Link.js";
 
 
 const Header = styled.h2`
 	padding: 0;
-	margin: 16px 0;
+	margin-top: 16px;
+	margin-bottom: 0;
 	color: ${ colors.$color_pink_dark };
 	font-weight: 50;
 	font-size: 1.5em;
@@ -20,43 +22,39 @@ const SupportContainer = styled.div`
 	flex-grow: 1;
 `;
 
-const Block = styled.div`
-	
+const TextImageContainer = styled.div`
+	display: flex;
+	align-items: top;
+
+	p {
+		margin-top: 0;
+	}
 `;
 
-const TextBlock = styled( Block )`
-	vertical-align: top;
-	max-width: 60%;
-	margin-top: 1em;
-	display: inline-block;
+const BoldLink = styled( Link )`
+	font-weight: bold;
 `;
 
 const Image = styled.img`
-	max-width: 35%;
+	max-width: 30%;
 	display: inline;
-	float: bottom;
-`;
-
-const Description = styled.p`
-	margin: 0;
-	white-space: normal;
-	max-width: 100%;
+	margin-left: 8px;
 `;
 
 const messages = defineMessages( {
 	supportKb: {
 		id: "support.kb",
-		defaultMessage: "The Yoast Knowlegde base is always a good place to start if you have a problem or a question. " +
-		"You can check the list of articles, or easily search the Knowledge base to find the answers you're looking for.",
+		defaultMessage: "The Yoast Knowlegde Base is always a good place to start if you have a problem or a question. " +
+		"You can check the list of articles, or easily search the knowledge base to find the answers you're looking for.",
 	},
 	supportForums: {
 		id: "support.forums",
-		defaultMessage: "On support forums at wordpress.org you can post about your issues, " +
-		"and others will help you out. For example:",
+		defaultMessage: "On the support forums at wordpress.org you can post about your issues, " +
+		"and others will help you out.",
 	},
 	supportForumsDetail: {
 		id: "support.forumsDetail",
-		defaultMessage: "To let others help you, please include as much detail to your description as possible.",
+		defaultMessage: "To let others help you, please include as much detail in your description as possible.",
 	},
 } );
 
@@ -77,39 +75,49 @@ const SupportCard = () => {
 					defaultMessage={ "Support" }
 				/>
 			</Header>
-			<FormattedMessage
-				id={ "support.card.paragraph" }
-				defaultMessage={ "If you have a question, if you need help, or just want to contact us, " +
-				"there are several ways to get in touch." }
-			/>
+			<p>
+				<FormattedMessage
+					id={ "support.card.paragraph" }
+					defaultMessage={ "If you have a question, if you need help, or if you just want to contact us, " +
+					"there are several ways to get in touch." }
+				/>
+			</p>
 			<SupportContainer>
-				<div>
-					<TextBlock>
-						<a href={ "https://kb.yoast.com/" } target="blank" >{ "Knowlegde base »" }</a>
-						<Description>
-							<FormattedMessage { ...messages.supportKb } />
-						</Description>
-					</TextBlock>
+				<BoldLink to={ "https://kb.yoast.com/" } linkTarget={ "_blank" }>
+					<FormattedMessage
+						id={ "support-card-kb-link" }
+						defaultMessage={ "Knowlegde base { arrows }" }
+						values={ {
+							arrows: "»",
+						} }
+					/>
+				</BoldLink>
+				<TextImageContainer>
+					<p>
+						<FormattedMessage { ...messages.supportKb } />
+					</p>
 					<Image src={ KbImage } />
-				</div>
-				<div>
-					<TextBlock>
-						<a href={ "https://kb.yoast.com/" } target="blank" >{ "Knowlegde base »" }</a>
-						<Description>
+				</TextImageContainer>
+				<BoldLink to={ "https://kb.yoast.com/kb/support/#free" } linkTarget={ "_blank" }>
+					<FormattedMessage
+						id={ "support-card-support-forums-link" }
+						defaultMessage={ "Free support forums { arrows }" }
+						values={ {
+							arrows: "»",
+						} }
+					/>
+				</BoldLink>
+				<TextImageContainer>
+					<div>
+						<p>
 							<FormattedMessage { ...messages.supportForums } />
-							<ul>
-								<li>
-									<a href={ "https://wordpress.org/support/plugin/wordpress-seo" } target="blank" > Yoast SEO </a>
-								</li>
-								<li>
-									<a href={ "https://wordpress.org/support/plugin/wordpress-seo" } target="blank" > All Free Yoast Plugins </a>
-								</li>
-							</ul>
+						</p>
+						<p>
 							<FormattedMessage { ...messages.supportForumsDetail } />
-						</Description>
-					</TextBlock>
+						</p>
+					</div>
 					<Image src={ ForumsImage } />
-				</div>
+				</TextImageContainer>
 			</SupportContainer>
 		</Fragment>
 	);
