5005f43b2bcd79cfe0999e594753fd6dd8ecebca
Anton Timmermans <email@atimmer.com>
2018-11-12 10:46:51 +0100

    Add redux devtools support

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import rootReducer from "./reducers";
 import {
 	getAccessToken,
@@ -38,9 +38,12 @@ if ( process.env.NODE_ENV === "development" ) {
 	middleware.push( createLogger() );
 }
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 export const store = createStore(
 	rootReducer,
-	applyMiddleware( ...middleware )
+	composeEnhancers(
+		applyMiddleware( ...middleware )
+	)
 );
 
 /**
