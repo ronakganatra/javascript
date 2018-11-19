a05e2aaa8da216e71bc9d5fdf6ffe0f11c05697b
Maarten Leenders <maarten@yoast.com>
2018-11-19 13:38:31 +0100

    Add test

---
+++ __tests__/components/account/subscriptions/SubscriptionsRow-test.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import { createComponentWithIntl } from "../../../../utils";
+import SubscriptionRow from "../../../../src/components/account/subscriptions/SubscriptionRow";
+
+
+let subscriptionsArray = [
+	{
+		id: "1",
+		icon: "icon.png",
+		name: "name",
+		used: 0,
+		limit: 2,
+		subscriptionNumber: "subscriptionNumber",
+		requiresManualRenewal: false,
+		hasNextPayment: true,
+		nextPayment: new Date( "1-1-2028" ),
+		hasEndDate: false,
+		endDate: null,
+		billingAmount: "1",
+		billingCurrency: "USD",
+		status: "active",
+		hasSites: true,
+		product: {
+			glNumber: 1,
+			productGroups: {
+				parentId: 1,
+			}
+		},
+	},
+	{
+		id: "1",
+		icon: "icon.png",
+		name: "name",
+		used: 1,
+		limit: 2,
+		subscriptionNumber: "subscriptionNumber2",
+		requiresManualRenewal: false,
+		hasNextPayment: true,
+		nextPayment: new Date( "1-1-2028" ),
+		hasEndDate: false,
+		endDate: null,
+		billingAmount: "1",
+		billingCurrency: "USD",
+		status: "active",
+		hasSites: true,
+		product: {
+			glNumber: 1,
+			productGroups: {
+				parentId: 1,
+			}
+		},
+	},
+];
+
+test( 'the SubscriptionRow component matches the snapshot', () => {
+	const component = createComponentWithIntl(
+		<SubscriptionRow
+			subscriptionsArray={ subscriptionsArray }
+			onManage={ () => {} }
+			isGrouped={ true }
+		/>
+	);
+
+	let tree = component.toJSON();
+	expect( tree ).toMatchSnapshot();
+} );
