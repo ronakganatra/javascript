35d47b9883d1371579e27c7ff53db15910e1f173
Andrea Fercia <a.fercia@gmail.com>
2018-12-11 14:28:08 +0100

    ESLint.

--- src/components/account/subscriptions/SubscriptionRow.js
+++ src/components/account/subscriptions/SubscriptionRow.js
@@ -146,6 +146,13 @@ const Separator = styled.div`
  * @constructor
  */
 class SubscriptionRow extends React.Component {
+	/**
+	 * Initializes the class with the specified props.
+	 *
+	 * @param {Object} props The props to be passed to the class that was extended from.
+	 *
+	 * @returns {void}
+	 */
 	constructor( props ) {
 		super( props );
 
@@ -362,14 +369,26 @@ class SubscriptionRow extends React.Component {
 
 		let previousBackgroundColor = this.props.background;
 
-		// In the expandable part of the row, background colors should be slightly darker versions of the original zebra colors.
-		const determineNextBackgroundColor = ( previousBackgroundColor ) => {
-			const darkerGrey = "#E5E5E5";
-			const darkerWhite = "#F0F0F0";
+		/**
+		 * Sets the expandable rows background color.
+		 *
+		 * In the expandable part of the row, background colors should be slightly
+		 * darker versions of the original zebra colors.
+		 *
+		 * @returns {string} The background color to apply.
+		 */
+		const determineNextBackgroundColor = () => {
+			const darkerGrey = "#e5e5e5";
+			const darkerWhite = "#f0f0f0";
 
 			return previousBackgroundColor === darkerGrey ? darkerWhite : darkerGrey;
 		};
 
+		/**
+		 * Toggles the open state of the collapsible row.
+		 *
+		 * @returns {void}
+		 */
 		const toggleOpen = () => {
 			this.setState( { isOpen: ! this.state.isOpen } );
 		};
@@ -490,9 +509,11 @@ SubscriptionRow.propTypes = {
 	subscriptionsArray: PropTypes.array.isRequired,
 	isGrouped: PropTypes.bool.isRequired,
 	background: PropTypes.string,
-	intl: intlShape,
+	intl: intlShape.isRequired,
 };
 
-SubscriptionRow.defaultProps = {};
+SubscriptionRow.defaultProps = {
+	background: "",
+};
 
 export default injectIntl( SubscriptionRow );
