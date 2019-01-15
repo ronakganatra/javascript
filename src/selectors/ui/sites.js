e812e8c0ff94724ac4c096be1fae15e48b304e0a
Maarten Leenders <maarten@yoast.com>
2019-01-15 16:04:45 +0100

    Fix lint errors caused by this PR only

--- src/selectors/ui/sites.js
+++ src/selectors/ui/sites.js
@@ -9,6 +9,12 @@ export function getSitesUi( state ) {
 	return state.ui.sites;
 }
 
+/**
+ * Returns the retrievingSites state.
+ *
+ * @param   {Object}  state The application state.
+ * @returns {boolean}       The retrievingSites state (should be true when get sites request is not successful or failed yet).
+ */
 export const isRetrievingSites = ( state ) => {
 	return state.ui.sites.retrievingSites;
 };
