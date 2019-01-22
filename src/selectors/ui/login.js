fc05a485e3f8868a0a478746e543afe22e8701e7
Bram <bintzandt@users.noreply.github.com>
2019-01-22 06:54:11 +0100

    Refactor the ResetPassword container to use selectors.

--- src/selectors/ui/login.js
+++ src/selectors/ui/login.js
@@ -7,3 +7,13 @@
  * @returns {Object} The current login status.
  */
 export const getLogin = ( state ) => state.ui.login;
+
+/**
+ * Get the reset password from the state.
+ *
+ * @function
+ *
+ * @param {Object } state Application state.
+ * @returns {Object} The reset password object.
+ */
+export const getResetPassword = state => state.ui.resetPassword;
