4af8a6aca845b11456afb61510df25132f8f4ec5
Maarten Leenders <maarten@yoast.com>
2019-01-29 10:21:19 +0100

    Move cookie helpers to separate file

--- src/functions/connect.js
+++ src/functions/connect.js
@@ -1,29 +1,4 @@
-import Cookies from "js-cookie";
-
-export function makeSetCookie( key, expiresIn = false ) {
-	return ( value ) => {
-		if ( expiresIn ) {
-			console.log( expiresIn );
-			Cookies.set( key, value, {
-				expires: expiresIn,
-			} );
-		} else {
-			Cookies.set( key, value );
-		}
-	};
-}
-
-export function makeGetCookie( key ) {
-	return () => {
-		return Cookies.get( key );
-	};
-}
-
-export function makeHasCookie( key ) {
-	return () => {
-		return !! Cookies.get( key );
-	};
-}
+import { makeGetCookie, makeHasCookie, makeSetCookie } from "./cookieHelpers";
 
 export const setConnectClientId = makeSetCookie( "connectClientId", 1 / 24 );
 export const setConnectUrl = makeSetCookie( "connectUrl", 1 / 24 );
