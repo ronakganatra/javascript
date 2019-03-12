6317ae9910fbdc9070d9a2fbe2c8f7055a685b60
Diede Exterkate <Diede@yoast.com>
2019-03-12 12:45:37 +0100

    Run yarn test in non-interactive mode so it can be used in the lerna test cmd

--- packages/my-yoast-client/scripts/test.js
+++ packages/my-yoast-client/scripts/test.js
@@ -6,18 +6,21 @@ process.env.REACT_APP_API_URL = 'https://my.yoast.com/api';
 // if this file is missing. dotenv will never modify any environment variables
 // that have already been set.
 // https://github.com/motdotla/dotenv
-require('dotenv').config({silent: true});
+require( 'dotenv' ).config( { silent: true } );
 
-const jest = require('jest');
-const argv = process.argv.slice(2);
+const jest = require( 'jest' );
+const argv = process.argv.slice( 2 );
 
-// Watch unless on CI or in coverage mode
-if (!process.env.CI && argv.indexOf('--coverage') < 0) {
-  argv.push('--watch');
+
+const interactive = process.env.NOT_INTERACTIVE !== "true";
+
+// Watch unless on CI, nonInteractive or in coverage mode
+if ( interactive && ! process.env.CI && argv.indexOf( '--coverage' ) < 0 ) {
+	argv.push( '--watch' );
 }
 
-if ( process.env.CI ) {
-  argv.push( '--runInBand' );
+if ( ! interactive || process.env.CI ) {
+	argv.push( '--runInBand' );
 }
 
-jest.run(argv);
+jest.run( argv );
